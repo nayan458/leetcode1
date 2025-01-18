a2b7883aa662603f4814b20e8af3ad0dfe8ba82a
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-18 22:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (12.93%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,29 +1,60 @@
 class Solution {
-     public double myPow(double x, int n) {
-        long m = Math.abs((long)n); 
-        double result = 1.0;
-        double currentProduct = x;
-        
-        while (m > 0) {
-            if (m % 2 == 1) {
-                result *= currentProduct;
-            }
-            currentProduct *= currentProduct;
-            m /= 2;
+    public double myPow(double x, int n) {
+        if(n==0)
+            return 1;
+        if(x == 0)
+            return 0;
+        long m = n;
+        if(n<0){
+            m = Math.abs((long)n);
+            x=1/x;
         }
-        return n < 0 ? 1 / result : result;
+        double half= myPow(x,(int)(m/2));
+        if(m%2==1)
+            return x*half*half;
+        return half* half;
     }
-    // public double myPow(double x, int n) {
-    //     double ans;
-    //     if( n < 0)
-    //         x = 1/x;
-    //     if( n == 0) 
-    //         return 1;
-    //     if( Math.abs(n) == 1)
-    //         return x;
-    //     ans = myPow(x, Math.abs(n/2));
-    //     if(n%2 != 0)
-    //         return ans * ans * x;
-    //     return ans * ans;
-    // }
 }
+
+
+// class Solution {
+//     public double myPow(double x, int n) {
+//         long m = Math.abs(n);
+//         if(n < 0)
+//             x = 1/x;
+//         while(m > 0){
+//             x *= x;
+//             --m;
+//         }
+//         return x;
+//     }
+// }
+
+
+// class Solution {
+//      public double myPow(double x, int n) {
+//         long m = Math.abs((long)n); 
+//         double result = 1.0;
+//         double currentProduct = x;
+//         while (m > 0) {
+//             if (m % 2 == 1)
+//                 result *= currentProduct;
+//             currentProduct *= currentProduct;
+//             m /= 2;
+//         }
+//         return n < 0 ? 1 / result : result;
+//     }
+//     // public double myPow(double x, int n) {
+//     //     double ans;
+//     //     if( n < 0)
+//     //         x = 1/x;
+//     //     if( n == 0) 
+//     //         return 1;
+//     //     if( Math.abs(n) == 1)
+//     //         return x;
+//     //     ans = myPow(x, Math.abs(n/2));
+//     //     if(n%2 != 0)
+//     //         return ans * ans * x;
+//     //     return ans * ans;
+//     // }
+// }
