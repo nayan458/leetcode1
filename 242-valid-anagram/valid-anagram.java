679552a731a223289cb91ee72bbb9a7b858dd489
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-18 17:17:35 +0530

    Time: 16 ms (25.27%) | Memory: 44.7 MB (34.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,35 +1,3 @@
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if (s.length()!= t.length())
-//             return false;
-//         HashMap<Character,Integer> hm = new HashMap<>();
-//         for(int i=0; i<s.length(); i++){
-//             // if(hm.getOrDefault(s.charAt(i),1)>0){
-//                 int temp=0;
-//                 temp=hm.getOrDefault(s.charAt(i),0);
-//                 hm.put(s.charAt(i),++temp);
-//             // }
-                
-//         }
-//         System.out.println(hm);
-//         for(int i=0; i<s.length(); i++){
-//             if(hm.containsKey(t.charAt(i))){
-//                 int temp = hm.get(t.charAt(i));
-//                 hm.put(t.charAt(i),--temp);
-//                 if(temp <= 0)
-//                     hm.remove(t.charAt(i));
-//                 continue;
-//             }
-//             break;
-//         }
-//         System.out.println(hm);
-//         if(hm.size() == 0){
-//             return true;
-//         }
-        
-//         return false;
-//     }
-// }
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> frequencyCount = new HashMap<>();
@@ -40,8 +8,6 @@ class Solution {
             frequencyCount.put(s.charAt(i),frequencyCount.getOrDefault(s.charAt(i),0)+1);
         int count = 0;
         for(int i = 0; i < m; i++) {
-            // if(frequencyCount.getOrDefault(t.charAt(i),0)==0)
-            //     return false;
             count = frequencyCount.getOrDefault(t.charAt(i),0);
             frequencyCount.put(t.charAt(i),--count);
             if(count < 0)
