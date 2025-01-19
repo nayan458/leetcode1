13dea04e4f44fc94f0d12a3c3d5014435815610a
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-19 21:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (62.89%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n = nums.length;
-        int countDecreasing = 0;
-
-        for (int i = 0; i < n; i++) {
-            // Check if the current element is greater than the next element (modulo n)
-            if (nums[i] > nums[(i + 1) % n]) {
-                countDecreasing++;
-            }
-            // If more than one decreasing point is found, it's not a valid rotated sorted array
-            if (countDecreasing > 1) {
+        int drop = 0;
+        for(int i = 0; i < nums.length ; i++){
+            if(nums[i] > nums[(i+1) % nums.length])
+                ++drop;
+            if(drop > 1)
                 return false;
-            }
         }
-
-        return true;
+        return drop > 1 ? false : true;
     }
 }
