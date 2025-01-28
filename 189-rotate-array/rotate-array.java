2695b7b953ff063d4993b8b4f3a12e7fb3c3d37a
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-28 14:30:11 +0530

    Time: 4 ms (6.21%) | Memory: 57.5 MB (35.14%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -6,31 +6,15 @@ class Solution {
         nums[j] = temp;
     }
 
-    public void printSolution(int[] nums) {
-        System.out.print('[');
-        for(int j = 0; j < nums.length; j++)
-            System.out.printf("%d,",nums[j]);
-        System.out.println(']');
-    }
-
     public void rotate(int[] nums, int k) {
         int n = nums.length,l = 0, r = ((l+k) % n);
         int numberToInsert = nums[l],temp = nums[r];
         k = k%n;
         for(int i = 0; i < n/2; i++)
             swap(nums,i,n-i-1);
-        
         for( int i = 0; i < k/2; i++ )
             swap(nums,i,k-i-1);
-
         for( int i = 0,j = n-1; i < (n-k)/2; i++ ,--j)
             swap(nums,i+k,j);
-        
-        
-        // for(int i = 0; i < n; i++){
-        //     swap(nums, r, 0);
-        //     r = ((r + k) % n);
-        //     printSolution(nums);
-        // }
     }
 }
