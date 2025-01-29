890e27eeccaf12dc33e8d0546ad8d31920f8ecab
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-29 12:36:43 +0530

    Time: 51 ms (28.53%) | Memory: 73.5 MB (59.81%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -5,10 +5,6 @@ class Solution {
         // sort the array
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
         
-        for (int[] row : intervals) {
-            System.out.println(Arrays.toString(row));
-        }
-
         int i = 0, j = 0, breakPoint = intervals[0][1];
         while(i<intervals.length){
             int start = intervals[i][0];
@@ -19,9 +15,7 @@ class Solution {
                 ++j;
                 breakPoint = Math.min(breakPoint,intervals[i][1]);
             }
-
             ++i;
-
         }
 
         return j;
