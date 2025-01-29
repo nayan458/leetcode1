ee55d5d010cbd2b8bbdc08eec7ce0ff84c1ee4d2
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-29 11:11:50 +0530

    Time: 8 ms (87.05%) | Memory: 47 MB (16.11%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,7 +2,7 @@ class Solution {
     public int[][] merge(int[][] intervals) {
 
         // initialize array to store the result
-        ArrayList<List<Integer>> result = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
 
         // sort the array
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
@@ -10,9 +10,7 @@ class Solution {
 
         int i = 0, j = 0, breakPoint = intervals[0][1];
         while(i<intervals.length){
-
-            ArrayList<Integer> l = new ArrayList<>();
-            l.add(intervals[i][0]);
+            int start = intervals[i][0];
             breakPoint = intervals[i][1];
 
             while(i + 1 < intervals.length && intervals[i + 1][0] <= breakPoint){
@@ -22,18 +20,48 @@ class Solution {
 
             ++i;
 
-            l.add(breakPoint);
-            result.add(l);
+            result.add(new int[]{start, breakPoint});
         }
 
-
-        // 
-        int[][] array = new int[result.size()][];
-        for (i = 0; i < result.size(); i++) {
-            List<Integer> row = result.get(i);
-            array[i] = row.stream().mapToInt(Integer::intValue).toArray();
-        }
-
-        return array;
+        return result.toArray(new int[result.size()][]);
     }
 }
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+
+//         // initialize array to store the result
+//         ArrayList<List<Integer>> result = new ArrayList<>();
+
+//         // sort the array
+//         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        
+
+//         int i = 0, j = 0, breakPoint = intervals[0][1];
+//         while(i<intervals.length){
+
+//             ArrayList<Integer> l = new ArrayList<>();
+//             l.add(intervals[i][0]);
+//             breakPoint = intervals[i][1];
+
+//             while(i + 1 < intervals.length && intervals[i + 1][0] <= breakPoint){
+//                 ++i;
+//                 breakPoint = Math.max(intervals[i][1],breakPoint);
+//             }
+
+//             ++i;
+
+//             l.add(breakPoint);
+//             result.add(l);
+//         }
+
+
+//         // 
+//         int[][] array = new int[result.size()][];
+//         for (i = 0; i < result.size(); i++) {
+//             List<Integer> row = result.get(i);
+//             array[i] = row.stream().mapToInt(Integer::intValue).toArray();
+//         }
+
+//         return array;
+//     }
+// }
