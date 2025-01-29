bc7c9a52baaec4dfb4c9e1a6a22c10366963c9e7
Nayanmoni Baruah <nayancoding@gmail.com>
2025-01-29 12:01:49 +0530

    Time: 1 ms (98.03%) | Memory: 45.7 MB (15.36%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -2,11 +2,10 @@ class Solution {
     public int[] buildArray(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-        for(int i = 0; i < n; i++) 
-        // {
+        for(int i = 0; i <= n/2; i++) {
             ans[i] = nums[nums[i]];
-            // ans[n-i-1]
-        // }
+            ans[n-i-1] = nums[nums[n-i-1]];
+        }
 
         return ans;
     }
