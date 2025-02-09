0edf5c8b0ba98f71ca36dbb6cd77ea91c2b9a866
Nayanmoni Baruah <nayancoding@gmail.com>
2025-02-09 13:05:22 +0530

    Time: 3 ms (92.53%) | Memory: 43.3 MB (81.31%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,29 +1,52 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> hm = new HashMap<>();
-        for(int i = 0; i < nums2.length; i++)
-            hm.put(nums2[i],i);
-        
-        System.out.println(hm);
-        for(int i = 0; i < nums1.length; i++){
-            
-            int indexInNums2 = hm.get(nums1[i]);    // get the index of the element in the array nums2
-            int elem = nums1[i];
-            nums1[i] = -1;
-
-            // int j = 0;
-            // for(j = 0; j < nums2.length; j++) {
-            //     if(nums2[j] == elem)
-            //         break;
-            // }
-
-            for(int j = indexInNums2; j < nums2.length; j++) {
-                if(nums2[j] > elem){
-                    nums1[i] = nums2[j];
-                    break;
-                }
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0; i < nums2.length; i++) {
+            // if(s.isEmpty() || s.peek() > num2[i]) 
+            //     s.push(nums2[i]);
+            // else {
+            while(!s.isEmpty() && nums2[i] > s.peek()) {
+                hm.put(s.pop(),nums2[i]);
             }
+            s.push(nums2[i]);
+            // }
         }
-        return nums1;    
+        while(!s.isEmpty()) {
+            hm.put(s.pop(),-1);
+        }
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = hm.get(nums1[i]);
+        }
+        return nums1;
     }
 }
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         Map<Integer, Integer> hm = new HashMap<>();
+//         for(int i = 0; i < nums2.length; i++)
+//             hm.put(nums2[i],i);
+        
+//         System.out.println(hm);
+//         for(int i = 0; i < nums1.length; i++){
+            
+//             int indexInNums2 = hm.get(nums1[i]);    // get the index of the element in the array nums2
+//             int elem = nums1[i];
+//             nums1[i] = -1;
+
+//             // int j = 0;
+//             // for(j = 0; j < nums2.length; j++) {
+//             //     if(nums2[j] == elem)
+//             //         break;
+//             // }
+
+//             for(int j = indexInNums2; j < nums2.length; j++) {
+//                 if(nums2[j] > elem){
+//                     nums1[i] = nums2[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return nums1;    
+//     }
+// }
