7d94eb903233a84e2392e682216e47caffd21ba5
Nayanmoni Baruah <nayancoding@gmail.com>
2025-03-14 12:04:58 +0530

    Time: 4 ms (28.38%) | Memory: 42.8 MB (19.71%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -3,42 +3,26 @@ import java.util.*;
 class Solution {
     public String removeDuplicateLetters(String s) {
         Map<Character, Integer> hm = new HashMap<>();
-        Stack<Character> st = new Stack<>();
+        StringBuilder st = new StringBuilder();
         Set<Character> set = new HashSet<>();
-        String str = new String();
-        String result = new String();
 
-        for (int i = s.length() - 1; i >= 0; --i) {
+        for (int i = s.length() - 1; i >= 0; --i)
             hm.putIfAbsent(s.charAt(i), i);
-        }
 
         for (int i = 0; i < s.length(); i++) {
-            
-            if (set.contains(s.charAt(i)) || (!st.empty() && st.peek() == s.charAt(i))) 
+
+            if (set.contains(s.charAt(i))) 
                 continue;
 
-            if (st.empty() || st.peek() < s.charAt(i)) {
-                st.push(s.charAt(i));
-                set.add(s.charAt(i));
-            } else {
-                while (!st.empty() && st.peek() > s.charAt(i)) {
-                    if (hm.get(st.peek()) > i) {
-                        set.remove(st.pop());
-                    } else {
-                        break;
-                    }
-                }
-                set.add(s.charAt(i));
-                st.push(s.charAt(i));
+            while (st.length() > 0 && st.charAt(st.length() - 1) > s.charAt(i) && hm.get(st.charAt(st.length() - 1)) > i) {
+                    set.remove(st.charAt(st.length() - 1));
+                    st.deleteCharAt(st.length() - 1);
             }
+
+            set.add(s.charAt(i));
+            st.append(s.charAt(i));
         }
 
-        while (!st.empty())
-            str += st.pop();
-
-        for (int i = str.length() - 1; i >= 0; i--)
-            result += str.charAt(i);
-
-        return result;
+        return st.toString();
     }
 }
