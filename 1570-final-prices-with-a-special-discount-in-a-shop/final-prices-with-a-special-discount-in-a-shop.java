0995b7bf800d037ee86c876b29b6f126034f1d96
Nayanmoni Baruah <nayancoding@gmail.com>
2025-05-11 00:05:58 +0530

    Time: 3 ms (54.81%) | Memory: 43.7 MB (73.80%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,20 +1,34 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        Stack<Pair<Integer,Integer>> stk = new Stack<>();
+        Stack<Integer> stk = new Stack<>();
         int i = 0;
-
         while(i < prices.length){
-
-            while(!stk.isEmpty() && (stk.peek().getValue() >= prices[i])) {
-                Pair p = stk.pop();
-                prices[(int)p.getKey()] = (int)p.getValue() - prices[i];
-            }
-
-            stk.push(new Pair(i,prices[i]));
-            
+            while(!stk.isEmpty() && (prices[stk.peek()] >= prices[i]))
+                prices[stk.pop()] -=prices[i];
+            stk.push(i);
             ++i;
         }
 
         return prices;
     }
 }
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         Stack<Pair<Integer,Integer>> stk = new Stack<>();
+//         int i = 0;
+
+//         while(i < prices.length){
+
+//             while(!stk.isEmpty() && (stk.peek().getValue() >= prices[i])) {
+//                 Pair p = stk.pop();
+//                 prices[(int)p.getKey()] = (int)p.getValue() - prices[i];
+//             }
+
+//             stk.push(new Pair(i,prices[i]));
+
+//             ++i;
+//         }
+
+//         return prices;
+//     }
+// }
