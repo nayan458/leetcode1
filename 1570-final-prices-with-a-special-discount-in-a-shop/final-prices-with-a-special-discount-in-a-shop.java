7471caf278a23520145f0131ebdc893763bb57bf
Nayanmoni Baruah <nayancoding@gmail.com>
2025-05-11 00:10:29 +0530

    Time: 1 ms (99.06%) | Memory: 43.6 MB (73.80%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,17 +1,43 @@
+//  TIME COMPLEXITY O(N^2)
+// Although it's time complexity is higher but it is good for smaller input constrains
 class Solution {
     public int[] finalPrices(int[] prices) {
-        Stack<Integer> stk = new Stack<>();
-        int i = 0;
-        while(i < prices.length){
-            while(!stk.isEmpty() && (prices[stk.peek()] >= prices[i]))
-                prices[stk.pop()] -=prices[i];
-            stk.push(i);
-            ++i;
+        int n=prices.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++)
+            if(prices[i]>=prices[j]){
+                prices[i]-=prices[j];
+                break;
+            }
         }
-
         return prices;
     }
 }
+
+
+
+//  TIME COMPLEXITY O(N)
+
+// // Not using PAIR and using KISS
+// class Solution {
+//     public int[] finalPrices(int[] prices) {
+//         Stack<Integer> stk = new Stack<>();
+//         int i = 0;
+//         while(i < prices.length){
+//             while(!stk.isEmpty() && (prices[stk.peek()] >= prices[i]))
+//                 prices[stk.pop()] -=prices[i];
+
+//             stk.push(i);
+
+//             ++i;
+//         }
+//         return prices;
+//     }
+// }
+
+// USING PAIRS
+
+
 // class Solution {
 //     public int[] finalPrices(int[] prices) {
 //         Stack<Pair<Integer,Integer>> stk = new Stack<>();
