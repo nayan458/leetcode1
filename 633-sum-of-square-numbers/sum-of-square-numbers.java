7235450e6f2aaa9cd40c0805fa9f1afd7a31e79e
Nayanmoni Baruah <nayancoding@gmail.com>
2025-06-10 12:31:13 +0530

    Time: 4 ms (57.02%) | Memory: 41.2 MB (5.24%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long left = 0, right = (long)Math.sqrt(c);
+        while(left <= right ){
+            if(left*left + right*right == c)return true;
+            if( left*left + right*right < c) ++left;
+            else --right;
+        }
+        return false;
+    }
+}
