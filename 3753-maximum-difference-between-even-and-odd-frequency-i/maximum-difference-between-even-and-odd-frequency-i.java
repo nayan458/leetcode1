6764e943c3edebf216acf23ce5da7ca0b48a4366
Nayanmoni Baruah <nayancoding@gmail.com>
2025-06-10 11:17:20 +0530

    Time: 3 ms (54.69%) | Memory: 42.9 MB (28.38%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.java
@@ -3,8 +3,7 @@ class Solution {
         
         Map<Character, Integer> hm = new HashMap<>();
 
-        int maxOdd = Integer.MIN_VALUE + 1, maxEven = Integer.MIN_VALUE;
-        int minOdd = Integer.MAX_VALUE, minEven = Integer.MAX_VALUE - 1;
+        int maxOdd = Integer.MIN_VALUE, minEven = Integer.MAX_VALUE - 1;
         
         for(int i = 0; i < s.length(); i++)
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i),0) + 1);
@@ -12,21 +11,16 @@ class Solution {
         for (Map.Entry<Character,Integer> mapElement : hm.entrySet()) {
             int v = mapElement.getValue();
             if(v % 2 == 0) {
-                if(maxEven < v)
-                    maxEven = v;
                 if(minEven > v)
                     minEven = v;
             } else {
                 if(maxOdd < v)
                     maxOdd = v;
-                if(minOdd > v)
-                    minOdd = v;
             }
         }
-        
-        maxOdd -= minEven;
-        minOdd -= maxEven;
 
-        return maxOdd > minOdd ? maxOdd : minOdd;
+        maxOdd -= minEven;
+
+        return maxOdd;
     }
 }
