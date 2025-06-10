24208b0724edc11a3daef53e54af44bb6fe9278f
Nayanmoni Baruah <nayancoding@gmail.com>
2025-06-10 14:36:03 +0530

    Time: 4 ms (83.03%) | Memory: 68.4 MB (48.28%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,59 +1,80 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+// SOLUTION: I
+// USING STRING
+
+// Traverse the list and convert into a single string O(n)
+// Traverse the string and check for palindrom        O(n)
+// Time Complaxity: O(n)
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+
+//         if(head == null || head.next == null)
+//             return true;
+//         ListNode ptr = head;
+//         String s = "";
+
+//         while(ptr != null) {
+//             String str = Integer.toString(ptr.val);
+//             s += str;
+//             ptr = ptr.next;
+//         }
+
+//         int left = 0, right = s.length() - 1;
+
+//         while(left < right) {
+//             if(s.charAt(left) != s.charAt(right))
+//                 return false;
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+// }
+
+
+// SOLUTION: II
+// SPLITING THE LIST AND REVERSING IT
+
+// find the mid O(n/2)
+// split in two O(1)
+// reverse the sencond list O(n/2)
+// compare the two list O(n/2)
+// Time Complexity O(n/2) = O(n)
+
 class Solution {
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-        while (current != null) {
-            ListNode nextTemp = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextTemp;
-        }
-        return prev;
-    }
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
+        if (head == null || head.next == null) return true;
+        
+        // 1. Find middle
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        if (fast != null) {
-            slow = slow.next;
+        
+        // 2. Reverse second half
+        ListNode secondHalf = reverse(slow.next);
+        
+        // 3. Compare
+        while (secondHalf != null) {
+            if (head.val != secondHalf.val) return false;
+            head = head.next;
+            secondHalf = secondHalf.next;
         }
-
-        ListNode secondHalfHead = reverseList(slow);
-        ListNode firstHalfCurrent = head;
-
-        boolean isPal = true;
-        while (secondHalfHead != null) {
-            if (firstHalfCurrent.val != secondHalfHead.val) {
-                isPal = false;
-                break;
-            }
-            firstHalfCurrent = firstHalfCurrent.next;
-            secondHalfHead = secondHalfHead.next;
-        }
-
-        return isPal;
+        
+        return true;
     }
-        
-        
-
-        }         
     
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
