a52e2c4e879d027a4ac0f770c7ad59e712c64083
Nayanmoni Baruah <nayancoding@gmail.com>
2025-06-10 14:24:09 +0530

    Time: 4 ms (83.03%) | Memory: 59.4 MB (72.63%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,26 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-
-        if(head == null || head.next == null)
-            return true;
-        ListNode ptr = head;
-        String s = "";
-
-        while(ptr != null) {
-            String str = Integer.toString(ptr.val);
-            s += str;
-            ptr = ptr.next;
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
         }
-
-        int left = 0, right = s.length() - 1;
-
-        while(left < right) {
-            if(s.charAt(left) != s.charAt(right))
-                return false;
-            left++;
-            right--;
-        }
-
-        return true;
+        return prev;
     }
-}
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        ListNode secondHalfHead = reverseList(slow);
+        ListNode firstHalfCurrent = head;
+
+        boolean isPal = true;
+        while (secondHalfHead != null) {
+            if (firstHalfCurrent.val != secondHalfHead.val) {
+                isPal = false;
+                break;
+            }
+            firstHalfCurrent = firstHalfCurrent.next;
+            secondHalfHead = secondHalfHead.next;
+        }
+
+        return isPal;
+    }
+        
+        
+
+        }         
+    
