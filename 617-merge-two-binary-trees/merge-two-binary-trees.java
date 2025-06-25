e5996b414d462176a7152a6194c8348ea0f3631f
Nayanmoni Baruah <nayancoding@gmail.com>
2025-06-25 11:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (80.28%) - LeetSync

---
+++ 617-merge-two-binary-trees/merge-two-binary-trees.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // TreeNode root;
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        TreeNode t = null;
+        if(root1 != null && root2 != null)
+            t = new TreeNode((root1.val + root2.val));
+        else if(root1 == null)
+            return root2;
+        else
+            return root1;
+
+        t.left = mergeTrees(root1.left,root2.left);
+        t.right = mergeTrees(root1.right,root2.right);
+
+        return t;
+    }
+}
