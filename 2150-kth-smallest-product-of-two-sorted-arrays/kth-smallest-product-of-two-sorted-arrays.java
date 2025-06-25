a2bc9c84c1f45f06da689a3cf8fd563e8ecff007
Nayanmoni Baruah <nayancoding@gmail.com>
2025-06-25 23:33:29 +0530

    Time: 61 ms (100.00%) | Memory: 55.5 MB (39.92%) - LeetSync

--- 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
@@ -1,61 +1,89 @@
+import java.util.Arrays;
+
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long left = -10000000000L, right = 10000000000L;
-        long ans = 0;
+        int n1 = nums1.length, n2 = nums2.length;
 
-        while (left <= right) {
+        // Split nums1 and nums2 into positive and negative parts
+        int pivot1 = findFirstZero(nums1); // index of first non-negative in nums1
+        int pivot2 = findFirstZero(nums2); // index of first non-negative in nums2
+
+        int[] pos1 = Arrays.copyOfRange(nums1, pivot1, n1); // non-negatives from nums1
+        int[] pos2 = Arrays.copyOfRange(nums2, pivot2, n2); // non-negatives from nums2
+
+        int[] neg1 = reverseAndNegate(Arrays.copyOfRange(nums1, 0, pivot1)); // negatives as positive
+        int[] neg2 = reverseAndNegate(Arrays.copyOfRange(nums2, 0, pivot2)); // negatives as positive
+
+        // Total negative products = (neg1 × pos2) + (pos1 × neg2)
+        long negCount = (long) neg1.length * pos2.length + (long) pos1.length * neg2.length;
+
+        long sign = 1;
+        if (k > negCount) {
+            // We're looking for non-negative products
+            k -= negCount;
+        } else {
+            // Flip search to negative products using neg1 × neg2 and pos1 × pos2
+            int[] tmp = neg2;
+            neg2 = pos2;
+            pos2 = tmp;
+            sign = -1;
+            k = negCount - k + 1; // convert to kth largest negative
+        }
+
+        // Binary search to find the smallest product for given k
+        long left = 0, right = (long) 1e10;
+        while (left < right) {
             long mid = left + (right - left) / 2;
-            long count = countLessEqual(mid, nums1, nums2);
+            long total = countLessEqual(mid, neg1, neg2) + countLessEqual(mid, pos1, pos2);
 
-            if (count >= k) {
-                ans = mid;
-                right = mid - 1;
+            if (total >= k) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
 
-        return ans;
+        return left * sign;
     }
 
-    private long countLessEqual(long mid, int[] nums1, int[] nums2) {
+    // Counts number of pairs (a,b) such that a in n1, b in n2 and a*b <= max
+    private long countLessEqual(long max, int[] n1, int[] n2) {
         long count = 0;
-        for (int a : nums1) {
-            if (a == 0) {
-                if (mid >= 0) {
-                    count += nums2.length;
-                }
-            } else if (a > 0) {
-                // count how many b in nums2 such that a * b <= mid
-                int l = 0, r = nums2.length - 1, res = -1;
-                while (l <= r) {
-                    int m = (l + r) / 2;
-                    if ((long)a * nums2[m] <= mid) {
-                        res = m;
-                        l = m + 1;
-                    } else {
-                        r = m - 1;
-                    }
-                }
-                count += res + 1;
-            } else {
-                // a < 0
-                int l = 0, r = nums2.length - 1, res = nums2.length;
-                while (l <= r) {
-                    int m = (l + r) / 2;
-                    if ((long)a * nums2[m] <= mid) {
-                        res = m;
-                        r = m - 1;
-                    } else {
-                        l = m + 1;
-                    }
-                }
-                count += nums2.length - res;
+        int j = n2.length - 1;
+
+        for (int i = 0; i < n1.length; i++) {
+            while (j >= 0 && (long) n1[i] * n2[j] > max) {
+                j--;
             }
+            count += j + 1;
         }
         return count;
     }
+
+    // Finds the index of the first non-negative number
+    private int findFirstZero(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    // Reverse and negate the array to make it increasing positive
+    private int[] reverseAndNegate(int[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = -arr[arr.length - 1 - i];
+        }
+        return result;
+    }
 }
+
 
 // // Binary Search on Answer
 // class Solution {
