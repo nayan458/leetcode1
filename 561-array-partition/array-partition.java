d1a80e8895fc8e03fdd7356a265a8144896387ea
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-08 11:45:33 +0530

    Time: 8 ms (97.74%) | Memory: 46.4 MB (88.11%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,9 +1,30 @@
+// Approach II: Counting Sort
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
+        int []bucket = new int[20001];
         int sum = 0;
-        for(int i = 0; i < nums.length; i += 2)
-            sum += nums[i];
+        boolean take = true;
+        
+        Arrays.stream(nums).forEach(num -> bucket[num + 10000]++);
+
+        for( int i = 0; i < bucket.length; i++){
+            while(bucket[i] > 0){
+                if(take)
+                    sum += i - 10000;
+                take = !take;
+                --bucket[i];
+            }
+        }
         return sum;
     }
 }
+// Approach I: Simple Sorting
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         Arrays.sort(nums);
+//         int sum = 0;
+//         for(int i = 0; i < nums.length; i += 2)
+//             sum += nums[i];
+//         return sum;
+//     }
+// }
