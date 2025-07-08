44f8be8cdf71502899035d5a3b55c4628b8e9067
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-08 12:02:16 +0530

    Time: 5 ms (99.67%) | Memory: 46.7 MB (48.98%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,12 +1,11 @@
+// Approach II: Enhanced Counting Sort using max and min limits
 class Solution {
-    private int []bucket = new int[20001];
-    private int sum = 0;
-    private boolean take = true;
-    private int min, max;
-
     public int arrayPairSum(int[] nums) {
-        min = nums[0];
-        max = nums[0];
+        int []bucket = new int[20001];
+        int sum = 0;
+        boolean take = true;
+        int min = nums[0], max = nums[0];
+        
         for(int elem : nums){
             bucket[elem + 10000]++;
             min = Math.min(min, elem);
@@ -27,3 +26,33 @@ class Solution {
         return sum;
     }
 }
+// Approach II: Counting Sort: O(N)
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         int []bucket = new int[20001];
+//         int sum = 0;
+//         boolean take = true;
+
+//         Arrays.stream(nums).forEach(num -> bucket[num + 10000]++);
+
+//         for( int i = 0; i < bucket.length; i++){
+//             while(bucket[i] > 0){
+//                 if(take)
+//                     sum += i - 10000;
+//                 take = !take;
+//                 --bucket[i];
+//             }
+//         }
+//         return sum;
+//     }
+// }
+// Approach I: Simple Sorting O(Nlogn)
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         Arrays.sort(nums);
+//         int sum = 0;
+//         for(int i = 0; i < nums.length; i += 2)
+//             sum += nums[i];
+//         return sum;
+//     }
+// }
