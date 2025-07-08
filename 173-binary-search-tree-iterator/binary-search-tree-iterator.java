3a25d5ecb6c69faa5cd217fc6f5f0403a4565629
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-08 12:47:07 +0530

    Time: 17 ms (62.25%) | Memory: 48 MB (83.95%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.java
@@ -14,33 +14,28 @@
  * }
  */
 class BSTIterator {
-    List<Integer> q;
-    private static int i;
-    private static int size = 0;
+    Queue<TreeNode> q;
 
     public BSTIterator(TreeNode root) {
-        q = new ArrayList<>();
+        q = new LinkedList<>();
         inorder(root);
-        i = 0;
-        size = q.size();
     }
     
     public int next() {
         if(hasNext())
-            return q.get(i++);
+            return q.poll().val;
         return -1;
     }
     
     public boolean hasNext() {
-        System.out.printf("i: %d,\tsize: %d",i,size);
-        return i < size;
+        return !q.isEmpty();
     }
 
     private void inorder(TreeNode root){
         if(root == null)
             return;
         inorder(root.left);
-        q.add(root.val);
+        q.add(root);
         inorder(root.right);
 
     }
@@ -52,52 +47,3 @@ class BSTIterator {
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class BSTIterator {
-//     Queue<TreeNode> q;
-
-//     public BSTIterator(TreeNode root) {
-//         q = new LinkedList<>();
-//         inorder(root);
-//     }
-    
-//     public int next() {
-//         if(hasNext())
-//             return q.poll().val;
-//         return -1;
-//     }
-    
-//     public boolean hasNext() {
-//         return !q.isEmpty();
-//     }
-
-//     private void inorder(TreeNode root){
-//         if(root == null)
-//             return;
-//         inorder(root.left);
-//         q.add(root);
-//         inorder(root.right);
-
-//     }
-// }
-
-// /**
-//  * Your BSTIterator object will be instantiated and called as such:
-//  * BSTIterator obj = new BSTIterator(root);
-//  * int param_1 = obj.next();
-//  * boolean param_2 = obj.hasNext();
-//  */
