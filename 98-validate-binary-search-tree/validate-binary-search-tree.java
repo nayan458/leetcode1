93b497e9a276641cae91098899f7e7b1af337280
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-09 14:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (78.40%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,19 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    private long min = Long.MIN_VALUE;    
-
+    private TreeNode min = null;
     public boolean isValidBST(TreeNode root) {
-        if(root == null)
-            return true;
-
-        if( !isValidBST(root.left))
+        if(root == null) return true;
+        boolean left = isValidBST(root.left);
+        if(min != null && min.val >= root.val)
             return false;
-
-        if(root.val <= min)
-            return false;
-
-        min = root.val;
-
-        return isValidBST(root.right);
+        min = root;
+        boolean right = isValidBST(root.right);
+        return left && right;
     }
 }
+// class Solution {
+
+//     private long min = Long.MIN_VALUE;    
+
+//     public boolean isValidBST(TreeNode root) {
+//         if(root == null)
+//             return true;
+
+//         if( !isValidBST(root.left))
+//             return false;
+
+//         if(root.val <= min)
+//             return false;
+
+//         min = root.val;
+
+//         return isValidBST(root.right);
+//     }
+// }
