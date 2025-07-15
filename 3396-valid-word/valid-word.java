0386357c7d0c4fa1993eafa7bd1b1580319480ee
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-15 15:02:29 +0530

    Time: 1 ms (99.12%) | Memory: 41.9 MB (80.13%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,12 +1,12 @@
 class Solution {
     private static Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U'));
+    private boolean hasVowel = false;
+    private boolean hasConsonant = false;
 
     public boolean isValid(String word) {
         if(word.length() < 3)
             return false;
 
-        boolean hasVowel = false;
-        boolean hasConsonant = false;
 
         for (int i = 0; i < word.length(); i++){
             char ch = word.charAt(i);
