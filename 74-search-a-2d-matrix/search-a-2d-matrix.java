c4b58e387fdd9b2f9f7f758159529c70c068f2ac
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-18 13:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (38.60%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,22 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix[0].length, m = matrix.length;
-        for(int i = 0; i < m; i++){
-            if(matrix[i][n - 1] == target)
+        int l = 0, r = m - 1;
+        int mid = 0, m_index = r;
+        while(l <= r){
+            mid = l + ((r - l)/2);
+            if(matrix[mid][n - 1] == target)
                 return true;
-            if(matrix[i][n - 1] < target)
-                continue;
-            else
-                return binarySearch(matrix[i], target);
+            if(matrix[mid][n - 1] < target)
+                l = mid + 1;
+            else{
+                m_index = Math.min(m_index, mid);
+                r = mid - 1;
+            }
         }
-        return false;
+
+        return binarySearch(matrix[m_index], target);
+        // for(int i = 0; i < m; i++){
+        //     if(matrix[i][n - 1] == target)
+        //         return true;
+        //     if(matrix[i][n - 1] < target)
+        //         continue;
+        //     else
+        //         return binarySearch(matrix[i], target);
+        // }
+        // return false;
     }
 
     // private boolean binarySearchIndex(int[] arr)
 
     private boolean binarySearch(int[] arr, int target){
         int left = 0, right = arr.length - 1;
-        int mid = left + ((right - left)/2);
+        int mid;
         while(left <= right){
             mid = left + ((right - left)/2);
             if(arr[mid] == target)
