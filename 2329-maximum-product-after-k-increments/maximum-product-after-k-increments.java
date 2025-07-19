17efb2167bd7fc69d57aae3c6ed188ff7415a8ac
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-20 00:10:46 +0530

    Time: 288 ms (66.87%) | Memory: 57.2 MB (57.14%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
@@ -6,20 +6,8 @@ class Solution {
 
         for(int num : nums) heap.add(num);
 
-        // for(int i = 0; i < k; i++)
-        //     heap.add(heap.poll() + 1);
-
-        while(k > 0){
-            int current = heap.poll();
-            int next = heap.isEmpty() ? Integer.MAX_VALUE : heap.peek();
-
-            int gap = Math.min(k, next - current);
-
-            if(gap == 0)  gap = 1;
-            
-            heap.add(current + gap);
-            k -= gap;
-        }
+        for(int i = 0; i < k; i++)
+            heap.add(heap.poll() + 1);
         
         while(!heap.isEmpty())
             product = (product * heap.poll()) % MOD;
