94fc26ff45d1cd3443d97c2ae62585cc83971eb2
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-19 23:54:39 +0530

    Time: 291 ms (44.07%) | Memory: 57.1 MB (57.14%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
@@ -2,16 +2,9 @@ class Solution {
     private static int MOD = 1_000_000_007;
     public int maximumProduct(int[] nums, int k) {
         PriorityQueue<Integer> heap = new PriorityQueue<>();
-
         long product = 1;
-        int count_Zero = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(count_Zero > k)
-                return 0;
-            heap.add(nums[i]);
-            if(nums[i] == 0)
-                count_Zero++;
-        }
+
+        for(int num : nums) heap.add(num);
 
         for(int i = 0; i < k; i++)
             heap.add(heap.poll() + 1);
