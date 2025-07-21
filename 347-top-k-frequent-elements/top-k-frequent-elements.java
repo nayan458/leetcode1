8ebeeca9e1ebedc5ee7a3286550213a98dddfd65
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-21 12:57:33 +0530

    Time: 7 ms (99.04%) | Memory: 48.4 MB (73.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,29 +1,54 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int[] bucket = new int[20_001];
+        int min = 0, max = 0;
+        int[] ans = new int[k];
         PriorityQueue<int[]> heap = new PriorityQueue<>(
             (a,b) -> Integer.compare(a[1],b[1])
         );
-        Map<Integer, Integer> hm = new HashMap<>();
-        int[] ans = new int[k];
 
-        for(int elem : nums)
-            hm.put(elem, hm.getOrDefault(elem,0) + 1);
-        
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
+        for(int elem: nums){
+            bucket[elem + 1_000] += 1;
+            min = Math.min(min, elem);
+            max = Math.max(max, elem);
+        }
 
-            heap.add(new int[]{key,value});
-
+        for(int i = min +  1_000; i <= max + 1_000; i++){
+            heap.add(new int[]{i - 1_000, bucket[i]});
             if(heap.size() > k)
                 heap.poll();
         }
 
-        while(!heap.isEmpty()){
+        while(!heap.isEmpty())
             ans[--k] = heap.poll()[0];
-        }
-
         return ans;
-
     }
 }
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         PriorityQueue<int[]> heap = new PriorityQueue<>(
+//             (a,b) -> Integer.compare(a[1],b[1])
+//         );
+//         Map<Integer, Integer> hm = new HashMap<>();
+//         int[] ans = new int[k];
+
+//         for(int elem : nums)
+//             hm.put(elem, hm.getOrDefault(elem,0) + 1);
+        
+//         for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+//             Integer key = entry.getKey();
+//             Integer value = entry.getValue();
+
+//             heap.add(new int[]{key,value});
+
+//             if(heap.size() > k)
+//                 heap.poll();
+//         }
+
+//         while(!heap.isEmpty()){
+//             ans[--k] = heap.poll()[0];
+//         }
+
+//         return ans;
+//     }
+// }
