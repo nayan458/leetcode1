055fd596829ab3726aecf498cf668f23ef9088b4
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-22 11:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (8.17%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,28 +1,28 @@
+// Approach II: Binary Search
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int left = matrix[0][0]; // Smallest element
-        int right = matrix[n - 1][n - 1]; // Largest element
+        int left = matrix[0][0], right = matrix[n - 1][n - 1];
+        int mid;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int count = countLessEqual(matrix, mid, n);
-            if (count < k) {
+        while(left < right){
+            mid = left + (right - left)/2;
+            int count = countEquals(matrix, mid, n);
+            if(count < k)
                 left = mid + 1;
-            } else {
+            else
                 right = mid;
-            }
         }
 
         return left;
     }
 
-    private int countLessEqual(int[][] matrix, int target, int n) {
+    private int countEquals(int[][] matrix, int target, int n){
         int count = 0;
         int row = n - 1, col = 0;
 
-        while (row >= 0 && col < n) {
-            if (matrix[row][col] <= target) {
+        while(row >= 0 && col < n){
+            if(matrix[row][col] <= target){
                 count += (row + 1);
                 col++;
             } else {
