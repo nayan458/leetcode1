80ec787b5474fce1a8509fd52db1b8bbd98ef3bc
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-29 16:48:36 +0530

    Time: 72 ms (18.47%) | Memory: 45.5 MB (40.46%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        // heap or balance BST with the following structure
+        // [ [difference, row, col] ]
+        int m = heights.length;
+        int n = heights[0].length;
+        PriorityQueue<int[]> heap = new PriorityQueue<>(
+            (a,b) -> Integer.compare(a[0],b[0])
+        );
+
+        Set<Integer> visited = new HashSet<>(); // visited array of [row, col]
+        heap.add(new int[]{0, 0, 0});
+
+        while(!heap.isEmpty()){
+            int[] node = heap.poll();
+            int row = node[1];
+            int col = node[2];
+            int diff = node[0];
+            int index = (row * n) + col;
+
+            if(index == ((m - 1) * n + (n - 1))) return diff;
+            if(visited.contains(index)) continue;
+            visited.add(index);
+            
+            int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            for (int[] dir : directions) {
+                int dr = dir[0];
+                int dc = dir[1];
+                int newRow = row + dr, newCol = col + dc;
+                index = (newRow * n) + newCol;
+                if(newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || visited.contains(index))
+                    continue;
+
+                int newDiff = Math.max(diff,Math.abs(heights[row][col] - heights[newRow][newCol]));
+                heap.add(new int[]{newDiff, newRow, newCol});
+            }
+        }   
+        return 0;
+    }
+}
