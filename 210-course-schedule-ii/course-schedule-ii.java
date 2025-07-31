4339e6e9ea02d3f2396ecaa80dd2a5d24de83530
Nayanmoni Baruah <nayancoding@gmail.com>
2025-07-31 16:18:45 +0530

    Time: 5 ms (77.03%) | Memory: 45.9 MB (74.95%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,13 +1,15 @@
 class Solution {
-    Set<Integer> courseSchedule = new LinkedHashSet<>();
-    private Map<Integer, Set<Integer>> hm = new HashMap<>();
+    Set<Integer> courseScheduled = new LinkedHashSet<>();
+    int[] courseSchedule;
+    int index = 0;
     private Set<Integer> waiting = new HashSet<>();
 
     public int[] findOrder(int n, int[][] edges) {
         List<List<Integer>> adjList = createGraph(edges, n);
-        System.out.println(hm);
 
         boolean[] visited = new boolean[n];
+        index = n -  1;
+        courseSchedule = new int[n];
 
         for (int i = 0; i < n; i++) {
             if (!visited[i])
@@ -15,9 +17,10 @@ class Solution {
                     return new int[0];
         }
 
-        List<Integer> result = new ArrayList<>(courseSchedule);
-        Collections.reverse(result);
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return courseSchedule;
+        // List<Integer> result = new ArrayList<>(courseSchedule);
+        // Collections.reverse(result);
+        // return result.stream().mapToInt(Integer::intValue).toArray();
     }
 
     private boolean dfs(int node, List<List<Integer>> adjList, boolean[] visited) {
@@ -26,14 +29,15 @@ class Solution {
 
         waiting.add(node);
         for(int neighbour: adjList.get(node)){
-            if(!courseSchedule.contains(neighbour)){
+            if(!courseScheduled.contains(neighbour)){
                 if(dfs(neighbour, adjList, visited))
                     return true;
             } 
         }
 
         waiting.remove(node);
-        courseSchedule.add(node);
+        courseSchedule[index--] = node;
+        courseScheduled.add(node);
         return false;
     }
 
