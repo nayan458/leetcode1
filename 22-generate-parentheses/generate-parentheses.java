e285aea9d82debdfdc6cd3a29af87694449830bb
Nayanmoni Baruah <nayancoding@gmail.com>
2025-08-12 12:19:26 +0530

    Time: 1 ms (82.77%) | Memory: 43.3 MB (60.11%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,22 +1,27 @@
 class Solution {
 
     List<String> ans = new ArrayList<>();
+    int n;
 
     public List<String> generateParenthesis(int n) {
-        createParenthesis(new StringBuffer("("), 1, 0, n);
+        this.n = n;
+        createParenthesis(new StringBuffer("("), 1, 0);
         return ans;
     }
 
-    private void createParenthesis(StringBuffer str, int open, int close, int n) {
-        // breaking statement
+    private void createParenthesis(StringBuffer str, int open, int close) {
         if(close == n){
             ans.add(new String(str.toString()));
             return;
         }
 
-        if(open < n)
-            createParenthesis(new StringBuffer(str).append("("), open + 1, close, n);
-        if(close < open)
-            createParenthesis(new StringBuffer(str).append(")"), open, close + 1, n);
+        if(open < n){
+            createParenthesis(str.append("("), open + 1, close);
+            str.delete(str.length() - 1, str.length());
+        }
+        if(close < open){
+            createParenthesis(str.append(")"), open, close + 1);
+            str.delete(str.length() - 1, str.length());
+        }
     }
 }
