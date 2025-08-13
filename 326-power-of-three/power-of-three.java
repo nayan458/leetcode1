f016aa530b00a71648d1d1513efd00832a7b4dc3
Nayanmoni Baruah <nayancoding@gmail.com>
2025-08-13 17:02:50 +0530

    Time: 8 ms (84.41%) | Memory: 44.4 MB (28.08%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,25 +1,37 @@
 class Solution {
-    private int[] threePower = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683,59049, 177147, 531441, 1594323, 4782969,14348907, 43046721, 129140163, 387420489, 1162261467};
-
     
     public boolean isPowerOfThree(int n) {
         if(n <= 0)
             return false;
-
-        int left = 0, right = 19;
-        
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            int ans = threePower[mid];
-            if(ans == n)
-                return true;
-            if(ans < n)
-                left = mid + 1;
-            else
-                right = mid - 1;
+        while(n % 3 == 0){
+            n /= 3;
         }
-        return false;
+        return n == 1 || n == 3;
     }
-
-
 }
+
+// class Solution {
+//     private static final int[] threePower = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683,59049, 177147, 531441, 1594323, 4782969,14348907, 43046721, 129140163, 387420489, 1162261467};
+
+    
+//     public boolean isPowerOfThree(int n) {
+//         if(n <= 0)
+//             return false;
+
+//         int left = 0, right = 19;
+        
+//         while(left <= right){
+//             int mid = left + (right - left)/2;
+//             int ans = threePower[mid];
+//             if(ans == n)
+//                 return true;
+//             if(ans < n)
+//                 left = mid + 1;
+//             else
+//                 right = mid - 1;
+//         }
+//         return false;
+//     }
+
+
+// }
