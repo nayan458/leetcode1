c6da14b2b8123a81548f0aac0653505425ed852a
Nayanmoni Baruah <nayancoding@gmail.com>
2025-08-29 14:36:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (100.00%) - LeetSync

---
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        long n_even = (n/2), n_odd = (n - n_even);
+        long m_even = (m/2), m_odd = (m - m_even);
+        
+        return ((n_odd*m_even) + (n_even*m_odd));
+
+    }
+}
