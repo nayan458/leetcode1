514f3aa47214c8f826a30bdb0ae16c89412b1fd9
Nayanmoni Baruah <nayancoding@gmail.com>
2025-09-10 17:03:26 +0530

    Time: 13 ms (27.93%) | Memory: 40.9 MB (50.45%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -1,19 +1,130 @@
 class Solution {
+    long n, delay, forget, valid, x;
+    long[] t;
+    long MOD = 1_000_000_007;
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        int MOD = 1_000_000_007;
-        long[] dp = new long[n + 1];
-        dp[1] = 1; // Day 1 one person learns
+        this.n = n;
+        this.delay = delay;
+        this.forget = forget;
+        this.valid = n - forget;
+        this.t = new long[n+1];
+        this.x = forget - delay;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = i + delay; j < i + forget && j <= n; j++) {
-                dp[j] = (dp[j] + dp[i]) % MOD;
+        for(int i = 0; i <= n; i++)
+            t[i] = -1;
+        
+
+        return (int)(dp(1) % MOD);
+    }
+    private long dp(long day){
+        if(day > n)
+            return 0;
+        long count = 0;
+        if(day > valid)
+            count++;
+        for(long i = 0; i < x; i++){
+            long new_day = (i+day+delay);
+            if(new_day > n)
+                break;
+            if(t[(int)new_day] != -1)
+                count = ((count + t[(int)new_day]) % MOD);
+            else{
+                t[(int)new_day] = dp(new_day) % MOD;
+                count += t[(int)new_day] % MOD;
             }
         }
-
-        long ans = 0;
-        for (int i = n - forget + 1; i <= n; i++) {
-            if (i >= 1) ans = (ans + dp[i]) % MOD;
-        }
-        return (int) ans;
+        return t[(int)day] = count % MOD;
     }
 }
+
+// class Solution {
+//     long n, delay, forget, valid, x;
+//     long[] t;
+//     public int peopleAwareOfSecret(int n, int delay, int forget) {
+//         this.n = n;
+//         this.delay = delay;
+//         this.forget = forget;
+//         this.valid = n - forget;
+//         this.t = new long[n+1];
+//         this.x = forget - delay;
+
+//         for(int i = 0; i <= n; i++)
+//             t[i] = -1;
+
+//         return (int)(dp(1) % 1_000_000_007);
+//     }
+//     private long dp(long day){
+//         if(day > n)
+//             return 0;
+//         int count = 0;
+//         if(day > valid)
+//             count++;
+//         for(long i = 0; i < x; i++){
+//             long new_day = (i+day+delay);
+//             if(new_day > n)
+//                 break;
+//             if(t[(int)new_day] != -1)
+//                 count += t[(int)new_day];
+//             else{
+//                 t[(int)new_day] = dp(new_day);
+//                 count += t[(int)new_day];
+//             }
+//         }
+//         return t[(int)day] =count;
+//     }
+// }
+
+// class Solution {
+//     long n, delay, forget, valid, x;
+//     long[] t;
+//     public int peopleAwareOfSecret(int n, int delay, int forget) {
+//         this.n = n;
+//         this.delay = delay;
+//         this.forget = forget;
+//         this.valid = n - forget;
+//         this.t = new long[n+1];
+//         this.x = forget - delay;
+
+//         for(int i = 0; i <= n; i++)
+//             t[i] = -1;
+
+//         return (int)(dp(1) % 1_000_000_007);
+//     }
+//     private long dp(long day){
+//         if(day > n)
+//             return 0;
+//         int count = 0;
+//         if(day > valid)
+//             count++;
+//         for(long i = 0; i < x; i++){
+//             long new_day = (i+day+delay);
+//             if(new_day > n)
+//                 break;
+//             count += dp(new_day);
+//         }
+//         return count;
+//     }
+// }
+
+// class Solution {
+//     public int peopleAwareOfSecret(int n, int delay, int forget) {
+//         int mod = 1_000_000_007;
+//         int x = forget - delay;
+//         int valid = n - forget;
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(1);
+//         int count = 0;
+//         while(!q.isEmpty()){
+//             int elem = q.poll();
+//             if(elem > valid){
+//                 count++;
+//             }
+//             for(int i = 0; i < x; i++){
+//                 if(i+elem+delay > n)
+//                     break;
+//                 q.add(i+elem+delay);
+
+//             }
+//         }
+//         return count;
+//     }
