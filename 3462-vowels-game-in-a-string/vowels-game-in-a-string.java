ff285789cd3c3db9942e00ad8e3c9dace4acad93
Nayanmoni Baruah <nayancoding@gmail.com>
2025-09-12 10:38:57 +0530

    Time: 12 ms (28.87%) | Memory: 45.4 MB (92.65%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -5,32 +5,12 @@ class Solution {
 
     public boolean doesAliceWin(String s) {
         List<Integer> arr = new ArrayList<>();
-        int count = 0;
-        int odd = 0, even = 0;
 
         for(int i = 0; i< s.length(); i++){
             char ch = s.charAt(i);
-            if(vowel.contains(ch)) count++;
-            else{
-                if(count != 0){
-                    arr.add(count);
-                    if(count % 2 == 0)
-                        even++;
-                    else
-                        odd++;
-                    count = 0;
-                }
-            } 
+            if(vowel.contains(ch)) 
+                return true;
         }
-        if(count != 0){
-            arr.add(count);
-            if(count % 2 == 0)
-                even++;
-            else
-                odd++;
-            count = 0;
-        }
-        System.out.println(odd + even);
-        return odd + even != 0;
+        return false;
     }
 }
