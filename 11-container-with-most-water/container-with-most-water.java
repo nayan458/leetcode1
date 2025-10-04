57c56181117be55b5973a1ec57b14709c4895202
Nayanmoni Baruah <nayancoding@gmail.com>
2025-10-04 10:40:24 +0530

    Time: 2 ms (98.84%) | Memory: 58 MB (44.73%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,23 +1,30 @@
 class Solution {
-
-    public int water(int h1, int h2,int d) {
-        return Math.min(h1,h2) * d;
-    }
-
     public int maxArea(int[] height) {
-        int max_water = 0, curr_water = 0, d = height.length - 1;
-        int l = 0, r = d;
-        while(l < r) {
-            curr_water = water(height[l], height[r], d--);
-            if(curr_water > max_water)
-                max_water = curr_water;
-            if(height[l] <= height[r])
-                l++;
-            else
-                r--;
-        }
+        int l = 0;
+        int r = height.length - 1;
+        int curr = height[l+1];
+        int l_max = height[l];
+        int r_max = height[r];
+        int maxWater = 0;
+        int curMax = Math.min(l_max,r_max) * (r-l);
 
-        return max_water;
-        
+        while(l < r){
+            if(l_max <= r_max){
+                curr = l_max;
+                while(curr <= l_max && l < r){
+                    curr = height[++l];
+                }
+                l_max = Math.max(curr, l_max);
+                curMax = Math.max(curMax, Math.min(l_max,r_max) * (r-l));
+            } else {
+                curr = r_max;
+                while(curr <= r_max && l < r){
+                    curr = height[--r];
+                }
+                r_max = Math.max(curr, r_max);
+                curMax = Math.max(curMax, Math.min(l_max,r_max) * (r-l));
+            }
+        }
+        return curMax;
     }
 }
