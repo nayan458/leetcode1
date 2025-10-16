8a1a5152b3ef6f6aa14d506c3109819d6e407578
Nayanmoni Baruah <nayancoding@gmail.com>
2025-10-16 15:47:07 +0530

    Time: 93 ms (12.18%) | Memory: 61.1 MB (41.12%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
@@ -2,20 +2,23 @@ class Solution {
     public int findSmallestInteger(int[] nums, int value) {
         Map<Integer, Integer> hm = new HashMap<>();
 
-        for (int elem : nums) {
-            int mod = ((elem % value) + value) % value; // normalize negative mods
-            hm.put(mod, hm.getOrDefault(mod, 0) + 1);
+        for(int elem: nums){
+            int mod = ((elem % value) + value) % value;
+            hm.put(mod, hm.getOrDefault(mod,0) + 1);
         }
 
+        System.out.println(hm);
+
         int i = 0;
-        while (true) {
-            int mod = i % value;
-            if (hm.getOrDefault(mod, 0) > 0) {
-                hm.put(mod, hm.get(mod) - 1);
-                i++;
-            } else {
-                return i;
+
+        for(i = 0; i < 100_001; i++) {
+            if(hm.getOrDefault(i%value,0) > 0){
+                hm.put(i%value, hm.get(i % value) - 1);
+                continue;
             }
+            else
+                return i;
         }
+        return i;
     }
 }
