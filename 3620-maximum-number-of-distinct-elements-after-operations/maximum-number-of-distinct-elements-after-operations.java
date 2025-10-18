3dca4b62f5fd03f53c831c65e4e67590ec86d922
Nayanmoni Baruah <nayancoding@gmail.com>
2025-10-18 18:57:56 +0530

    Time: 21 ms (35.17%) | Memory: 58.1 MB (62.76%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int maxDistinctElements(int[] nums, int k) {
-        Arrays.sort(nums);
-        int ans = 0, prev = (int)-1e9;
-
-        for (int x : nums) {
-            int l = Math.max(x - k, prev + 1);
-            if (l <= x + k) {
-                prev = l;
-                ans++;
+    public int maxDistinctElements(int[] arr, int diff) {
+        if (arr.length == 0) return 0;
+        Arrays.sort(arr);
+        int count = 0;
+        int prev = Integer.MIN_VALUE >> 1;
+        for (int i = 0, n = arr.length; i < n; i++) {
+            int a = arr[i];
+            int low = a - diff;
+            int high = a + diff;
+            int x = prev + 1;
+            if (x < low) x = low;
+            if (x <= high) {
+                count++;
+                prev = x;
             }
         }
-        return ans;
+        return count;
     }
 }
