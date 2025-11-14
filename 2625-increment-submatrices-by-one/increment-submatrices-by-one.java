944d57c13a97f723d706afb14e74e6835075e166
Nayanmoni Baruah <nayancoding@gmail.com>
2025-11-14 11:40:49 +0530

    Time: 12 ms (73.68%) | Memory: 83.2 MB (7.02%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] diff = new int [n+1][n+1];
+        int[][] diff = new int [n][n];
+        int[][] grid = new int [n][n];
 
         for(int[] query: queries) {
             int row1 = query[0];
@@ -8,22 +9,59 @@ class Solution {
             int row2 = query[2];
             int col2 = query[3];
 
+            // topLeft
             diff[row1][col1] += 1;
-            diff[row2+1][col1] -= 1;
-            diff[row1][col2+1] -= 1;
-            diff[row2+1][col2+1] += 1;
-        }
 
-        int[][] grid = new int [n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int x1 = (i == 0) ? 0 : grid[i - 1][j];
-                int x2 = (j == 0) ? 0 : grid[i][j - 1];
-                int x3 = (i == 0 || j == 0) ? 0 : grid[i - 1][j - 1];
-                grid[i][j] = diff[i][j] + x1 + x2 - x3;
+            // top right + 1 step right
+            if(col2 + 1 < n)
+                diff[row1][col2+1] -= 1;
+
+            // bottomLeft + 1 step down
+            if(row2 + 1 < n)
+                diff[row2+1][col1] -= 1;
+
+            // bottom right + 1 step down + 1 step right
+            if(row2 + 1 < n && col2 + 1 < n)
+                diff[row2+1][col2+1] += 1;
+        }
+        int top = 0;
+        int left = 0;
+        int topRightCorner = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                top = getTop(grid,i,j);
+                left = getLeft(grid,i,j);
+                topRightCorner = getTopRightCorner(grid,i,j);
+                grid[i][j] = diff[i][j] + top + left - topRightCorner;
             }
         }
         return grid;
+    }
+    private int getTopRightCorner(int[][] grid, int i, int j) {
+        if(i == 0 || j == 0) 
+            return 0;
+        return grid[i-1][j-1];
+    }
+    private int getTop(int[][] grid, int i, int j) {
+        if(i == 0)
+            return 0;
+        return grid[i-1][j];
+    }
+
+    private int getLeft(int[][] grid, int i, int j) {
+        if(j == 0)
+            return 0;
+        return grid[i][j-1];
+    }
+
+    private void print(int grid[][]) {
+        for(int[] row: grid) {
+            for(int elem: row) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
