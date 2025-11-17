180742749f67b879a5dca8e4b51d39fca04d9c0c
Nayanmoni Baruah <nayancoding@gmail.com>
2025-11-17 08:39:54 +0530

    Time: 1 ms (99.71%) | Memory: 65.6 MB (5.71%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -4,15 +4,16 @@ class Solution {
         int l = -1;
 
         for(int i = 0; i < nums.length; i++) {
-            System.out.println("i: " + i + " l: " + l + " diff: " + diff);
-            if(nums[i] == 1 && l != -1) {
-                diff = i - l - 1;
-                if(diff < k)
-                    return false;
-                else
+            if(nums[i] == 1) {
+                if(l != -1) {
+                    diff = i - l - 1;
+                    if(diff < k)
+                        return false;
+                    else
+                        l = i;
+                }
+                else 
                     l = i;
-            } else if(nums[i] == 1 && l == -1) {
-                l = i;
             }
         }
 
