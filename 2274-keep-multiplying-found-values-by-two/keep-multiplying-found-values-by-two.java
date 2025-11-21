6ec1bf8e7791a834cc711c84160bf3c99770bd66
Nayanmoni Baruah <nayancoding@gmail.com>
2025-11-21 17:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (62.32%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,11 +1,11 @@
 class Solution {
-    // static {
-    //     int[] nums = new int[] {5,3,6,1,12};
-    //     for(int i = 0; i < 500; i++)
-    //         findFinalValue(nums, i);
-    // }
+    static {
+        int[] nums = new int[] {5,3,6,1,12};
+        for(int i = 0; i < 500; i++)
+            findFinalValue(nums, i);
+    }
     
-    public int findFinalValue(int[] nums, int original) {
+    public static int findFinalValue(int[] nums, int original) {
         boolean[] set = new boolean[1001];
         for(int elem: nums) {
             set[elem] = true;
