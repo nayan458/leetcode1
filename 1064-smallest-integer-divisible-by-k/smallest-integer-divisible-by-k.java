8c0494afb864042e705c5314721f4425ca787132
Nayanmoni Baruah <nayancoding@gmail.com>
2025-11-25 09:22:54 +0530

    Time: 1 ms (100.00%) | Memory: 42 MB (43.23%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
@@ -5,28 +5,17 @@ class Solution {
     }
 
     public static int smallestRepunitDivByK(int k) {
-        if((k & 1) == 0)
+        if((k & 1) == 0 || (k % 5 == 0))
             return -1;
 
-        Set<Integer> st = lastDigit(k);
-
-        if(!st.contains(1))
-            return -1;
         int i = 1;
         int count = 0;
 
-        while(i % k != 0 || count < Integer.MAX_VALUE){
-            if(i % k == 0)
-                return ++count;
+        while(i % k != 0){
             i = (((i * 10) % k) + 1) % k;
             count++;
         }
-        return -1;
-    }
-    private static Set<Integer> lastDigit(int k){
-        Set<Integer> set = new HashSet<>();
-        for(int i = 1; i < 11; i++)
-            set.add((k*i) % 10);
-        return set;
+
+        return ++count;
     }
 }
