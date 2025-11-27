9f96ac94fecd08bb7ddbe56523cd29fdc40b02a9
Nayanmoni Baruah <nayancoding@gmail.com>
2025-11-27 09:35:09 +0530

    Time: 121 ms (12.39%) | Memory: 97.9 MB (18.58%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,23 +1,25 @@
 class Solution {
-    private int m;
-    private int n;
-    private int k;
-    private int[][][] t;
-    private int MOD = 1_000_000_007;
-    public int numberOfPaths(int[][] grid, int k) {
-        this.k = k;
-        this.m = grid.length;
-        this.n = grid[0].length;
-        this.t = new int[m][n][k];
+    private static int MOD = 1_000_000_007;
+
+    static {
+        int[][] grid = {{5,2,4},{3,0,5},{0,7,2}};
+        for(int i = 1; i <= 500; i++)
+            numberOfPaths(grid,i);
+    }
+
+    public static int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] t = new int[m][n][k];
         for(int i = 0; i < m; i++)
             for(int j = 0; j < n; j++)
                 for(int l = 0; l < k; l++)
                     t[i][j][l] = -1;
 
-        return dfs(grid,0,0,0);
+        return dfs(grid,0,0,0,t,k,m,n);
     }
 
-    private int dfs(int[][] grid, int row, int col, int mod_sum) {
+    private static int dfs(int[][] grid, int row, int col, int mod_sum, int[][][] t, int k, int m, int n) {
         if(col == n || row == m)
             return 0;
         mod_sum = ((mod_sum % k) + (grid[row][col] % k)) % k;
@@ -27,8 +29,8 @@ class Solution {
         if(row == m-1 && col == n-1)
             return t[row][col][mod_sum] = (mod_sum == 0) ? 1 : 0;
 
-        int rightPath = dfs(grid, row, col + 1, mod_sum);
-        int leftPath = dfs(grid, row + 1, col, mod_sum);
+        int rightPath = dfs(grid, row, col + 1, mod_sum, t, k, m, n);
+        int leftPath = dfs(grid, row + 1, col, mod_sum, t, k, m, n);
         return t[row][col][mod_sum] = (rightPath + leftPath) % MOD;
     }
 }
