2c2d8bdb9f8ff536fada91320142fbe5db421232
Nayanmoni Baruah <nayancoding@gmail.com>
2025-11-28 17:21:38 +0530

    Time: 26 ms (67.74%) | Memory: 99.6 MB (9.68%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -1,39 +1,38 @@
 class Solution {
-    private int k;
-    private int[] values;
-    private boolean[] visited;
-    private int count;
+    private static int count;
 
-    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
-        this.k = k;
-        this.values = values;
-        this.visited = new boolean[n];
-        this.count = 0;
-
+    static {
+        int n = 5;
+        int[][] edges = {{0,2},{1,2},{1,3},{2,4}}; 
+        int[] values = {1,8,1,4,4};
+        int k = 6;
+        for(int i = 0; i < 500; i++)
+            maxKDivisibleComponents(n, edges, values, k);
+    }
+    
+    public static int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        count = 0;
         List<List<Integer>> adjList = createAdj(edges, n);
-        dfs(adjList, 0, -1);
+        dfs(adjList, 0, -1, k, values);
         return count;
     }
-    private int dfs(List<List<Integer>> adjList, int node, int parent) {
-        if(parent == node)
-            return 0;
-        
-        int sum = values[node];
 
+    private static int dfs(List<List<Integer>> adjList, int node, int parent, int k, int[] values) {
+        if(parent == node)
+            return 0;    
+        int sum = values[node];
         for(int neighbor: adjList.get(node)){
             if(neighbor != parent)
-                sum = ((sum % k) + (dfs(adjList, neighbor, node) % k)) % k;
+                sum = ((sum % k) + (dfs(adjList, neighbor, node, k, values) % k)) % k;
         }
-
         if(sum % k == 0){
             count++;
             return 0;
         }
-
         return sum % k;
     }
 
-    private List<List<Integer>> createAdj(int[][] edges, int n){
+    private static List<List<Integer>> createAdj(int[][] edges, int n){
         List<List<Integer>> adjList = new ArrayList<>();
         for(int i = 0; i < n; i++)
             adjList.add(new ArrayList<>());
