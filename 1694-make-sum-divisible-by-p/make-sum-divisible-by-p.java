a91625d1b03c71eedeb6187f435a8eccac5dca73
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-01 10:41:27 +0530

    Time: 30 ms (33.40%) | Memory: 99.2 MB (34.04%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -1,28 +1,30 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long total = 0;
-        for (int x : nums) total = (total + x) % p;
-
-        if (total == 0) return 0;  
-        long target = total;
-
-        Map<Long, Integer> map = new HashMap<>();
-        map.put(0L, -1);
-        
-        long prefix = 0;
+        int need = sum(nums, p) % p;
+        if(need == 0)
+            return 0;
+        Map<Integer,Integer> hm = new HashMap<>();
+        hm.put(0,-1);
+        int curIdx = 0;
         int ans = nums.length;
-
-        for (int i = 0; i < nums.length; i++) {
-            prefix = (prefix + nums[i]) % p;
-            long need = (prefix - target + p) % p;
-
-            if (map.containsKey(need)) {
-                ans = Math.min(ans, i - map.get(need));
+        int remainder = 0;
+        for(int elem: nums) {
+            remainder = ((remainder % p) + (elem % p)) % p;
+            int target = (remainder - need + p) % p;
+            if(hm.containsKey(target)) {
+                int diff = curIdx - hm.get(target);
+                ans = Math.min(diff,ans);
             }
-
-            map.put(prefix, i);
+            hm.put(remainder,curIdx);
+            curIdx++;
         }
-
         return ans == nums.length ? -1 : ans;
+    }
+
+    private int sum(int[] nums, int p) {
+        int add = 0;
+        for(int elem: nums)
+            add = ((add % p) + (elem % p)) % p;
+        return add % p;
     }
 }
