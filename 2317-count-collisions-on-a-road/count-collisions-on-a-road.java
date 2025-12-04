e6989c655355a4212359115d3c0c0e94b2ab2cdb
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-04 09:38:42 +0530

    Time: 99 ms (18.14%) | Memory: 47.8 MB (12.90%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -0,0 +1,42 @@
+class Solution {
+    private Stack<Character> st;
+
+    public int countCollisions(String directions) {
+        st = new Stack<>();
+        int count = 0;
+
+        for(int i = 0; i < directions.length(); i++) {
+            char d = directions.charAt(i);
+            count += collisions(d);
+        }
+        return count;
+    }
+
+    private int collisions(char d) {
+        if(st.isEmpty() || d == 'R') {
+            st.push(d);
+            return 0;
+        }
+        switch(d) {
+            case 'L':
+                if(st.peek() == 'R') {
+                    st.pop();
+                    return 2 + collisions('S');
+                }
+                if(st.peek() == 'S')
+                    return 1;
+                return 0;
+            case 'S':
+                int count = 0;
+                while(!st.isEmpty() && st.peek() == 'R') {
+                    st.pop();
+                    count++;
+                }
+                st.push(d);
+                return count;
+            default :
+                break;
+        }
+        return 0;
+    }
+}
