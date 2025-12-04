c31eb4e1048fa547388c86d0f675029127ce5dd4
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-04 10:07:57 +0530

    Time: 99 ms (18.14%) | Memory: 47.9 MB (12.90%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,8 +1,4 @@
 class Solution {
-    static {
-        for(int i = 0; i < 500; i++)
-            countCollisions("LLRRSLR");
-    }
     private static Stack<Character> st;
 
     public static int countCollisions(String directions) {
