fe4ff4af0b30b9d1b4b9a77165a84dc11ca996e7
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-05 23:22:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (28.09%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,27 +1,12 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        boolean[] visited = new boolean[n+1];
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-
-        for(int i = 1; i*i <= n; i++) {
-            if(n % i == 0) {
-                if(visited[i])
-                    continue;
-                heap.add(i);
-                visited[i] = true;
-                if(visited[n/i])
-                    continue;
-                heap.add(n/i);
-                visited[n/i] = true;
-            }
-            while(heap.size() > k)
-                heap.poll();
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n%i == 0)
+                ++count;
+            if(count == k)
+                return i;
         }
-        while(heap.size() > k)
-            heap.poll();
-        System.out.println(heap);
-        if(heap.size() < k)
-            return -1;
-        return heap.poll();
+        return -1;
     }
 }
