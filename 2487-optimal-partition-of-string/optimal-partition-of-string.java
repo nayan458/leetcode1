8c3f252c2c86892b79ab34c3bda6885d4e9e64f6
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-05 23:33:13 +0530

    Time: 5 ms (99.28%) | Memory: 46.7 MB (41.21%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -1,5 +1,9 @@
 class Solution {
-    public int partitionString(String s) {
+    static {
+        for(int i = 0; i < 500; i++)
+            partitionString("abacaba");
+    }
+    public static int partitionString(String s) {
         boolean[] set = new boolean[26];
         int count = 0;
         for(int i = 0; i < s.length(); i++) {
