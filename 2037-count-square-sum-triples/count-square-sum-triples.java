ef6403e83bbd47ce45e641b3ede67f70fcb5f9ae
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-08 09:35:33 +0530

    Time: 38 ms (29.02%) | Memory: 47 MB (5.22%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -0,0 +1,34 @@
+class Solution {
+    private static final int[] square;
+    private static final Set<Integer> set;
+    static {
+        square = new int[251];
+        set = new HashSet<>();
+        for(int i = 0; i < 251; i++) {
+            square[i] = i*i;
+            set.add(square[i]);
+        }
+    }
+    public int countTriples(int n) {
+        int count = 0;
+        for(int i = 1; i <= n; i++)
+            for(int j = i+1; j <= n; j++) 
+                if(isTriplate(i,j,n)) {
+                    int c = square[i] + square[j];
+                    System.out.println(i + " " + j + " " + Math.sqrt(c));
+                    count++;
+                }   
+        // System.out.println(Arrays.toString(square));
+        return count * 2;
+    }
+
+    private boolean isTriplate(int a, int b, int n) {
+        int c = square[a] + square[b];
+        if(set.contains(c) && Math.sqrt(c) <= n)
+            return true;
+        return false;
+
+            
+    }
+}
+
