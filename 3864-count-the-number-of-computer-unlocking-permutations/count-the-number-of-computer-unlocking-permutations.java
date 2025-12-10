b35d0f9b48828fdc7ac6b956311b6e08e757f2cc
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-10 09:50:53 +0530

    Time: 3 ms (17.33%) | Memory: 92.3 MB (5.33%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -1,6 +1,13 @@
 class Solution {
     private static final int MOD = 1_000_000_007;
-    public int countPermutations(int[] complexity) {
+    
+    static {
+        int[] arr = {38,223,100,123,406,234,256,93,222,259,233,69,139,245,45,98,214};
+        for(int i = 0; i < 500; i++)
+            countPermutations(arr);
+    }
+
+    public static int countPermutations(int[] complexity) {
         int count = 1;
         if(noMaster(complexity)) return 0;
         int n = complexity.length;
@@ -9,7 +16,7 @@ class Solution {
         return count % MOD;
     }
 
-    private boolean noMaster(int[] arr) {
+    private static boolean noMaster(int[] arr) {
         int master = arr[0];
         for(int i = 1; i < arr.length; i++)
             if(arr[i] <= master)
