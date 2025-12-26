726731063e4d0c4831f0c6c947a159229c5a0dec
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-26 17:06:47 +0530

    Time: 8 ms (95.17%) | Memory: 47.3 MB (12.76%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,41 +1,17 @@
 class Solution {
     public int bestClosingTime(String customers) {
-
-    
-        int min = Integer.MAX_VALUE;
-        int index = -1;
-        int n = customers.length();
-        int[] pbc = new int[customers.length() + 1];
-        int[] pac = new int[customers.length() + 1];
-        int count = 0;
-
-        for(int k = 0; k< n ; k++){
-            if(customers.charAt(k) == 'N')
-                count++;
-            pbc[k+1] = count;
-        }
-        count = 0;
-        
-        for( int j = n-1; j>=0 ; j-- ){
-            if(customers.charAt(j) == 'Y')
-                count++;
-            pac[j]=count;
-        }
-        // Arrays.
-
-        // System.out.println(Arrays.toString(pac));
-        count = 0;
-        for(int i = 0; i<= customers.length() ; i++){
-            count = pbc[i] + pac[i]; // total penalty = pbc + pac
-            if(min > count){
-                min = count;
-                index = i;
+        int max = 0;
+        int idx = -1;
+        int curr = 0;
+        for(int i = 0; i < customers.length(); i++){
+            char ch = customers.charAt(i);
+            if(ch == 'N')   curr--;
+            else    curr++;
+            if(curr > max){
+                idx = i;
+                max = curr;
             }
         }
-            
-        return index == -1 ? 0 : index;
-
+        return idx == -1 ? 0 : idx+1;
     }
 }
-
-// 
