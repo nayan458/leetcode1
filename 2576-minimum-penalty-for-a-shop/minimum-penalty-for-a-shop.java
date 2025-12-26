d78904d816edbd6092a1a80cf821c55d0df4641e
Nayanmoni Baruah <nayancoding@gmail.com>
2025-12-26 16:23:25 +0530

    Time: 12 ms (62.41%) | Memory: 47.1 MB (20.69%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,25 +1,41 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
     public int bestClosingTime(String customers) {
-        int max = 0;
-        int idx = -1;
-        int curr = 0;
-        for(int i = 0; i < customers.length(); i++){
-            char ch = customers.charAt(i);
-            if(ch == 'N')   curr--;
-            else    curr++;
-            if(curr > max){
-                idx = i;
-                max = curr;
+
+    
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        int n = customers.length();
+        int[] pbc = new int[customers.length() + 1];
+        int[] pac = new int[customers.length() + 1];
+        int count = 0;
+
+        for(int k = 0; k< n ; k++){
+            if(customers.charAt(k) == 'N')
+                count++;
+            pbc[k+1] = count;
+        }
+        count = 0;
+        
+        for( int j = n-1; j>=0 ; j-- ){
+            if(customers.charAt(j) == 'Y')
+                count++;
+            pac[j]=count;
+        }
+        // Arrays.
+
+        // System.out.println(Arrays.toString(pac));
+        count = 0;
+        for(int i = 0; i<= customers.length() ; i++){
+            count = pbc[i] + pac[i]; // total penalty = pbc + pac
+            if(min > count){
+                min = count;
+                index = i;
             }
         }
-        return idx == -1 ? 0 : idx+1;
+            
+        return index == -1 ? 0 : index;
+
     }
 }
+
+// 
