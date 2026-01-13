b1a16313a690c216f8a14c658d57b4dab60aeae7
Nayanmoni Baruah <nayancoding@gmail.com>
2026-01-13 14:05:35 +0530

    Time: 131 ms (44.74%) | Memory: 149.3 MB (34.74%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.java
+++ 3763-separate-squares-i/separate-squares-i.java
@@ -1,44 +1,39 @@
 class Solution {
 
     public double separateSquares(int[][] squares) {
-        long totalArea = 0;
-        List<int[]> events = new ArrayList<>();
-
+        double max_y = 0;
+        double total_area = 0;
         for (int[] sq : squares) {
             int y = sq[1];
             int l = sq[2];
-            totalArea += (long) l * l;
-            events.add(new int[] { y, l, 1 });
-            events.add(new int[] { y + l, l, -1 });
+            total_area += (double) l * l;
+            max_y = Math.max(max_y, (double) (y + l));
         }
 
-        // sort by y-coordinate
-        events.sort((a, b) -> Integer.compare(a[0], b[0]));
-        double coveredWidth = 0; // sum of all bottom edges under the current scanning line
-        double currArea = 0; // current cumulative area
-        double prevHeight = 0; // height of the previous scanning line
-
-        for (int[] event : events) {
-            int y = event[0];
-            int l = event[1];
-            int delta = event[2];
-
-            int diff = y - (int) prevHeight;
-            // additional area between two scanning lines
-            double area = coveredWidth * diff;
-            // if this part of the area exceeds more than half of the total area
-            if (2L * (currArea + area) >= totalArea) {
-                return (
-                    prevHeight +
-                    (totalArea - 2.0 * currArea) / (2.0 * coveredWidth)
-                );
+        double lo = 0;
+        double hi = max_y;
+        double eps = 1e-5;
+        while (Math.abs(hi - lo) > eps) {
+            double mid = (hi + lo) / 2;
+            if (check(mid, squares, total_area)) {
+                hi = mid;
+            } else {
+                lo = mid;
             }
-            // update width: add width at the start event, subtract width at the end event
-            coveredWidth += delta * l;
-            currArea += area;
-            prevHeight = y;
         }
 
-        return 0.0;
+        return hi;
+    }
+
+    private Boolean check(double limit_y, int[][] squares, double total_area) {
+        double area = 0;
+        for (int[] sq : squares) {
+            int y = sq[1];
+            int l = sq[2];
+            if (y < limit_y) {
+                area += (double) l * Math.min(limit_y - y, (double) l);
+            }
+        }
+        return area >= total_area / 2;
     }
 }
