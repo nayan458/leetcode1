2a7a981bce6d85701076dc64615bc55ff653a35a
Nayanmoni Baruah <nayancoding@gmail.com>
2026-01-13 15:13:14 +0530

    Time: 131 ms (44.74%) | Memory: 149.3 MB (34.21%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.java
+++ 3763-separate-squares-i/separate-squares-i.java
@@ -1,31 +1,28 @@
 class Solution {
-
+    private double totalArea;
+    private double max;
     public double separateSquares(int[][] squares) {
-        double max_y = 0;
-        double total_area = 0;
-        for (int[] sq : squares) {
-            int y = sq[1];
-            int l = sq[2];
-            total_area += (double) l * l;
-            max_y = Math.max(max_y, (double) (y + l));
+        
+        for(int[] square: squares) {
+            int side = square[2];
+            int y = square[1];
+            totalArea += (double)side*side;
+            max = Math.max(max,y+side);
         }
-
-        double lo = 0;
-        double hi = max_y;
-        double eps = 1e-5;
-        while (Math.abs(hi - lo) > eps) {
-            double mid = (hi + lo) / 2;
-            if (check(mid, squares, total_area)) {
-                hi = mid;
-            } else {
-                lo = mid;
+        double l = 0;
+        double r = max;
+        while(Math.abs(r-l) > 1e-5) {
+            double mid = l + (r - l) /2;
+            if(check(mid, squares)){
+                r = mid;
             }
+            else
+                l = mid;
         }
-
-        return hi;
+        return r;
     }
 
-    private Boolean check(double limit_y, int[][] squares, double total_area) {
+    private Boolean check(double limit_y, int[][] squares) {
         double area = 0;
         for (int[] sq : squares) {
             int y = sq[1];
@@ -34,6 +31,6 @@ class Solution {
                 area += (double) l * Math.min(limit_y - y, (double) l);
             }
         }
-        return area >= total_area / 2;
+        return area >= totalArea / 2;
     }
 }
