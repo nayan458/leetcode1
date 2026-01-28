417dbdc7c2762c3beeb2ad49a7304b4bd7d5b9c3
Nayanmoni Baruah <nayancoding@gmail.com>
2026-01-28 12:00:27 +0530

    Time: 26 ms (46.29%) | Memory: 47.3 MB (10.87%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,16 +1,24 @@
 class Solution {
+    private int[][] t = new int[4001][21];
+    public Solution(){
+        for(int[] row: t)
+            Arrays.fill(row,-1);
+    }
+
     public int findTargetSumWays(int[] nums, int target) {
-        return isFound(nums,target,nums.length);
+        isFound(nums,target,nums.length);
+        return t[Math.abs(target*2)][nums.length];
     }
 
     private int isFound(int[] nums, int target, int idx) {
-        
         if(idx == 0)
             return target == 0 ? 1:0;
 
-        int include = isFound(nums, target - nums[idx-1],idx-1);
-        int exclude = isFound(nums, target - (-1*nums[idx-1]), idx -1);
+        if(t[Math.abs(target*2)][idx] != -1)  return t[Math.abs(target*2)][idx];
 
-        return include + exclude;
+        int positive = isFound(nums, target - nums[idx-1],idx-1);
+        int negative = isFound(nums, target - (-1*nums[idx-1]), idx -1);
+
+        return t[Math.abs(target*2)][idx] = positive + negative;
     }
 }
