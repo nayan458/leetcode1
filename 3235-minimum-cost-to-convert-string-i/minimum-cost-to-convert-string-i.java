a0bd9478a15d24e5bf86a7c177a0ce33fc7506d8
Nayanmoni Baruah <nayancoding@gmail.com>
2026-01-30 09:37:17 +0530

    Time: 219 ms (11.11%) | Memory: 48.2 MB (31.54%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -6,7 +6,7 @@ class Solution {
             int sch = Integerize(source.charAt(i));
             int tch = Integerize(target.charAt(i));
             if(sch == tch) continue;
-            if( minCost[sch][tch] == Long.MAX_VALUE ) return -1;
+            if( minCost[sch][tch] == 0 ) return -1;
             total += minCost[sch][tch];
         }
         return total;
@@ -22,41 +22,72 @@ class Solution {
         return minCost;
     }
 
-    private long[] Dijkstra(List<List<int[]>> adjList, int src) {
+    private long[] Dijkstra(List<List<int[]>> adjList, int src){
         long[] minCost = new long[26];
         boolean[] visited = new boolean[26];
 
-        Arrays.fill(minCost, Long.MAX_VALUE);
-        minCost[src] = 0;
+        // heap(node(id,val));
+        // we need the smallest valued node
+        PriorityQueue<long[]> heap = new PriorityQueue<>((a,b) -> Long.compare(a[1],b[1]));
 
-        PriorityQueue<long[]> heap =
-                new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
+        heap.offer(new long[]{src,0l});
 
-        heap.offer(new long[]{src, 0});
-
-        while (!heap.isEmpty()) {
+        while(!heap.isEmpty()) {
             long[] node = heap.poll();
-            int u = (int) node[0];
-            long cost = node[1];
+            int u = (int)node[0];
+            long val = node[1];
 
-            if (visited[u]) continue;
+            if(visited[u]) continue;
             visited[u] = true;
 
-            for (int[] edge : adjList.get(u)) {
+            for(int[] edge: adjList.get(u)) {
                 int v = edge[0];
-                int w = edge[1];
+                int wt = edge[1];
 
-                if (visited[v]) continue;
-
-                if (cost + w < minCost[v]) {
-                    minCost[v] = cost + w;
-                    heap.offer(new long[]{v, minCost[v]});
-                }
+                if(visited[v]) continue;
+                if(minCost[v] == 0 || minCost[v] > (val + wt))
+                    minCost[v] = val+wt;
+                heap.offer(new long[]{v,minCost[v]});
             }
         }
-
         return minCost;
     }
+
+    // private long[] Dijkstra(List<List<int[]>> adjList, int src) {
+    //     long[] minCost = new long[26];
+    //     boolean[] visited = new boolean[26];
+
+    //     Arrays.fill(minCost, Long.MAX_VALUE);
+    //     minCost[src] = 0;
+
+    //     PriorityQueue<long[]> heap =
+    //             new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
+
+    //     heap.offer(new long[]{src, 0});
+
+    //     while (!heap.isEmpty()) {
+    //         long[] node = heap.poll();
+    //         int u = (int) node[0];
+    //         long cost = node[1];
+
+    //         if (visited[u]) continue;
+    //         visited[u] = true;
+
+    //         for (int[] edge : adjList.get(u)) {
+    //             int v = edge[0];
+    //             int w = edge[1];
+
+    //             if (visited[v]) continue;
+
+    //             if (cost + w < minCost[v]) {
+    //                 minCost[v] = cost + w;
+    //                 heap.offer(new long[]{v, minCost[v]});
+    //             }
+    //         }
+    //     }
+
+    //     return minCost;
+    // }
 
     private List<List<int[]>> createAdjList(char[] uNode, char[] vNode, int[] cost){
         List<List<int[]>> adjList = new ArrayList<>();
