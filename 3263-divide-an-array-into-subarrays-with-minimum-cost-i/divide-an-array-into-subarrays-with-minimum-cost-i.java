f65224106c66642e489f1f42166376cabc07ce6c
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-02 09:45:02 +0530

    Time: 1 ms (99.21%) | Memory: 45.4 MB (59.13%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,17 +1,16 @@
 class Solution {
-    public int minimumCost(int[] A) {
-        int a = 51, b = 51;
-
-        for (int i = 1; i < A.length; i++) {
-            if (A[i] < a) {
-                b = a;
-                a = A[i];
-            } else if (A[i] < b)
-                b = A[i];
-
-            if (a == 1 && b == 1) break;
+    public int minimumCost(int[] nums) {
+        int a = nums[0];
+        int s1 = 51;
+        int s2 = 51;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] < s1){
+                s2 = s1;
+                s1 = nums[i];
+            }
+            else
+                s2 = Math.min(s2,nums[i]);
         }
-
-        return A[0] + a + b;
+        return a + s1 + s2;
     }
 }
