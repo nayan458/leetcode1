1dda3b67f32289234dab24a0ed8cb64c82a802be
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-05 14:11:04 +0530

    Time: 1 ms (98.72%) | Memory: 47 MB (13.78%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -3,15 +3,7 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
         for(int i = 0; i < n; i++)
-            result[i] = get(nums,i);
+            result[i] = nums[((i + nums[i]) % n + n) % n];
         return result;
-    }
-    private int get(int[] nums, int idx){
-        int r = Math.abs(nums[idx]);
-        int n = nums.length;
-        if(nums[idx] > 0)
-            return nums[(r+idx)%n];
-        return nums[ ((idx - r) % n + n) % n];
-
     }
 }
