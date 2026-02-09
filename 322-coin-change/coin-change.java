ca855ab1b3cb292337162a254f5a50f86e927d7a
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-09 23:04:30 +0530

    Time: 20 ms (37.33%) | Memory: 47.1 MB (30.70%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -4,20 +4,32 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         this.coins = coins;
         this.t = new int[coins.length + 1][amount+1];
-        dp(coins.length,amount);
+        init(t);
+        for(int i = 1; i <= coins.length; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(t[i][j] != 0) continue;
+                if(coins[i-1] > j) t[i][j] = t[i-1][j];
+                else t[i][j] = Math.min(1+t[i][j-coins[i-1]], t[i-1][j]);
+            }
+        }
         return t[coins.length][amount] == 10_005 ? -1 : t[coins.length][amount];
     }
 
-    private int dp(int idx, int amount) {
-        if(amount == 0) return 0;
-        if(idx == 0) return 10_005;
-        if(t[idx][amount] != 0) return t[idx][amount];
-        if(coins[idx -1] > amount)
-            return t[idx][amount] = dp(idx-1,amount);
-        else
-            return t[idx][amount] = Math.min(
-                (1+dp(idx,amount - coins[idx-1])),
-                dp(idx-1,amount)
-            );
+    private void init(int[][] t) {
+        for(int i = 1; i < t[0].length; i++)
+            t[0][i] = 10_005;
     }
+
+    // private int dp(int idx, int amount) {
+    //     if(amount == 0) return 0;
+    //     if(idx == 0) return 10_005;
+    //     if(t[idx][amount] != 0) return t[idx][amount];
+    //     if(coins[idx -1] > amount)
+    //         return t[idx][amount] = dp(idx-1,amount);
+    //     else
+    //         return t[idx][amount] = Math.min(
+    //             (1+dp(idx,amount - coins[idx-1])),
+    //             dp(idx-1,amount)
+    //         );
+    // }
 }
