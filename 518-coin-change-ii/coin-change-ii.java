bee49da75fc62e50ca87771ec3e0f2b4897f1b11
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-09 23:32:40 +0530

    Time: 19 ms (23.33%) | Memory: 56.5 MB (54.05%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -4,18 +4,35 @@ class Solution {
     public int change(int amount, int[] coins) {
         this.coins = coins;
         this.t = new int[coins.length+1][amount+1];
-        for(int i = 0; i < t.length; i++)
-            for(int j = 0; j < t[0].length; j++)
-                t[i][j] = -1;
-        return dp(coins.length,amount);
+        init();
+        for(int i = 1; i < t.length; i++)
+            for(int j = 1; j < t[0].length; j++) {
+                if(t[i][j] != -1) continue;
+                if(coins[i-1] > j) t[i][j] = t[i-1][j];
+                else t[i][j] = t[i][j - coins[i-1]] + t[i-1][j];
+            }
+        return t[coins.length][amount];
     }
 
-    private int dp(int idx, int amount) {
-        if(amount == 0) return 1;
-        if(idx == 0) return 0;
-        if(t[idx][amount] != -1) return t[idx][amount];
-        if(coins[idx-1] > amount) return dp(idx-1,amount);
-        else
-            return t[idx][amount] = dp(idx,amount-coins[idx-1]) + dp(idx-1,amount);
+    void init() {
+        for(int i = 1; i < t.length; i++)
+            for(int j = 1; j < t[0].length; j++)
+                t[i][j] = -1;
+        // without coin no amount other than zero is posible
+        for(int j = 1; j < t[0].length; j++)
+            t[0][j] = 0;
+        // all amount = 0 is posible
+        for(int i = 0; i < t.length; i++)
+            t[i][0] = 1;
+
     }
+
+    // private int dp(int idx, int amount) {
+    //     if(amount == 0) return 1;
+    //     if(idx == 0) return 0;
+    //     if(t[idx][amount] != -1) return t[idx][amount];
+    //     if(coins[idx-1] > amount) return dp(idx-1,amount);
+    //     else
+    //         return t[idx][amount] = dp(idx,amount-coins[idx-1]) + dp(idx-1,amount);
+    // }
 }
