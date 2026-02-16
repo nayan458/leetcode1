8bafe537a4d1921206e91a9627cc60fed3399361
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-16 16:44:18 +0530

    Time: 23 ms (42.40%) | Memory: 54.2 MB (33.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,11 +1,6 @@
 class Solution {
-    String text1;
-    String text2;
-    int[][] t;
     public int longestCommonSubsequence(String text1, String text2) {
-        this.text1 = text1;
-        this.text2 = text2;
-        this.t = new int[text1.length() + 1][text2.length() + 1];
+        int[][] t = new int[text1.length() + 1][text2.length() + 1];
 
         for(int i = 1; i <= text1.length(); i++) {
             for(int j = 1; j <= text2.length(); j++) {
