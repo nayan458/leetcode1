44142f0c249a2fb012a10636efa59a72e3127f26
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-18 00:15:12 +0530

    Time: 2 ms (95.79%) | Memory: 43.3 MB (98.05%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,21 +1,43 @@
 class Solution {
-    public List<String> readBinaryWatch(int k) {
-        if (k == 0) return List.of("0:00");
-        int mask = (1 << 6) - 1;
-        int q = (1 << k) - 1;
-        int limit = q << (10 - k);
-        List<String> res = new ArrayList<>();
+    int[] val = {8,4,2,1,32,16,8,4,2,1};
+    Set<String> set = new HashSet<>();
+    public List<String> readBinaryWatch(int turnedOn) {
+        backtracking(10,turnedOn,new ArrayList<>());
+        set.remove("");
+        return new ArrayList<>(set);
+    }
 
-        while (q <= limit) {
-            int min = q & mask;
-            int hour = q >> 6;
-            if (hour < 12 && min < 60)
-                //res.add(String.format("%d:%02d", hour, min));
-                res.add(hour + ":" + (min < 10 ? "0" : "") + min);
-            int r = q & -q;
-            int n = q + r;
-            q = (((q ^ n) / r) >> 2) | n;
+    private void backtracking(int idx,int turnedOn, List<Integer> arr){
+        if(idx == 0 && turnedOn > 0)
+            return;
+        if(turnedOn == 0) {
+            set.add(Stringify(arr));
+            return;
         }
-        return res;
+        // add
+        arr.add(idx-1);
+        // operation
+        backtracking(idx-1,turnedOn-1,arr);
+        // remove
+        arr.remove(arr.size()-1);
+        backtracking(idx-1,turnedOn,arr);
+    }
+
+    String Stringify(List<Integer> arr){
+        int hr = 0;
+        int m = 0;
+        for(int idx: arr)
+            if(idx > 3)
+                m += val[idx];
+            else
+                hr += val[idx];
+        if(hr > 11 || m > 59)
+            return "";
+        StringBuilder str = new StringBuilder();
+        str.append(String.valueOf(hr));
+        str.append(':');
+        if(m < 10) str.append('0');
+        str.append(String.valueOf(m));
+        return str.toString();        
     }
 }
