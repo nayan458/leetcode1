13a3aeaef97a15ae35f006897736719106ee6e25
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-22 13:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (67.94%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1;
+        int index = 0;
+        int maxGap = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    maxGap = Math.max(maxGap, index - last);
+                }
+                last = index;
+            }
+            n >>= 1;
+            index++;
+        }
+        return maxGap;
+    }
+}
