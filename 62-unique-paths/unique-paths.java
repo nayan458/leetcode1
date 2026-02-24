fb8e3f7eb7eecfc85b8eea46b6f6b868b3f33b1c
Nayanmoni Baruah <nayancoding@gmail.com>
2026-02-24 08:51:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,11 +2,6 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] directions = {{1,0},{0,1}};
         int[][] t = new int[m+1][n+1];
-        for(int i = 0; i <= m; i++)
-            for(int j = 0; j <= n; j++)
-                if(i == 0 || j == 0)
-                    t[i][j] = 0;
-        t[0][0] = 1;
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 if(i==1 && j==1)
