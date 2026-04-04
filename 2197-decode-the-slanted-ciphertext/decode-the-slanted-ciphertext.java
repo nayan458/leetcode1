81798dfdf3f2031d0f645900d0ade15aedbe5cac
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-04 13:02:17 +0530

    Time: 39 ms (26.04%) | Memory: 59.2 MB (24.26%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,38 +1,15 @@
 class Solution {
     public String decodeCiphertext(String encodedText, int rows) {
-        if(rows == 1)
-            return encodedText;
-
-        int len = encodedText.length();
-
-        if(len == 0)
-            return encodedText;
-
-        int cols = (int) Math.ceil( (double) len/rows);
-
-        char[][] matrix = new char[rows][cols];
-        char[] encodedArr = encodedText.toCharArray();
-        int e=0;
-
-        for(int i=0;i<rows;i++) {
-            for(int j=0;j<cols;j++) {
-                matrix[i][j] = encodedArr[e++];
+        StringBuilder str = new StringBuilder();
+        char[] strChar = encodedText.toCharArray();
+        int cols = (int)Math.ceil(encodedText.length()/rows);
+        for(int i = 0; i < cols; i++){
+            int idx = i;
+            while(idx < encodedText.length()){
+                str.append(strChar[idx]);
+                idx += cols+1;
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int startCol = 0; startCol < cols; startCol++) {
-            int r = 0;
-            int c = startCol;
-
-            while (r < rows && c < cols) {
-                sb.append(matrix[r][c]);
-                r++;
-                c++;
-            }
-        }
-
-        return sb.toString().stripTrailing();
+        return str.toString().stripTrailing();
     }
 }
