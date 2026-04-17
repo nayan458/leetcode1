bb34fd6012569e4a42a3853386c3942325a5466d
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-17 11:12:55 +0530

    Time: 51 ms (67.01%) | Memory: 94.9 MB (85.79%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.java
@@ -1,48 +1,28 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        int n =  nums.length;
+        Map<Integer, Integer> hm = new HashMap<>();
         int min = Integer.MAX_VALUE;
-        Map<Long,List<Integer>> hm = new HashMap<>();
 
-        for(int i = 0; i < n; i++)
-            hm.computeIfAbsent((long)(nums[i] * 1l), k -> new ArrayList<>()).add(i);
-        for(int i = 0; i < n-1;i++){
-            if(min == 1) return 1;
-            long rev = reverse(nums[i]);
-            if(!hm.containsKey(rev)) continue;
-            min = Math.min(min,minDiff(rev,hm.get(rev),i));
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                min = Math.min(min, i - hm.get(nums[i]));
+            }
+
+            int rev = reverse(nums[i]);
+            if (rev != -1) {
+                hm.put(rev, i);
+            }
         }
 
         return min == Integer.MAX_VALUE ? -1 : min;
     }
 
-    private int minDiff(long x, List<Integer> list, int idx){
-        int pos = upperBound(list, idx);
-        if(pos < list.size())
-            return list.get(pos) - idx;
-        return Integer.MAX_VALUE;
-    }
-
-    private long reverse(int x) {
+    private int reverse(int x) {
         long rev = 0;
         while (x > 0) {
             rev = rev * 10 + (x % 10);
             x /= 10;
         }
-        return rev;
-    }
-
-    private int upperBound(List<Integer> list, int target){
-        int l = 0, r = list.size();
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-
-            if (list.get(mid) > target) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return l;
+        return rev > Integer.MAX_VALUE ? -1 : (int) rev;
     }
 }
