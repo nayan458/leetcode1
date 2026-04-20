7fa10576d5087337b9d1a812e4329bc5858a18ef
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-20 12:48:41 +0530

    Time: 23 ms (37.35%) | Memory: 53.8 MB (80.61%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int[][] t = new int[m + 1][n + 1];
+    private String s1;
+    private String s2;
+    private int[][] t;
 
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(text1.charAt(i-1) == text2.charAt(j-1))
-                    t[i][j] = 1 + t[i-1][j-1];
-                else
-                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
-            }
-        }
-        return t[m][n];
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        this.s1 = text1;
+        this.s2 = text2;
+        this.t = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++)
+            for(int j = 0; j <= n; j++)
+                t[i][j] = -1;
+        return dp(m,n);
+    }
+    private int dp(int i, int j) {
+        if(i == 0 || j == 0)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        if(s1.charAt(i-1) == s2.charAt(j-1))
+            return t[i][j] = 1 + dp(i-1,j-1);
+        return t[i][j] = Math.max(
+            dp(i-1,j),dp(i,j-1)
+        );
     }
 }
