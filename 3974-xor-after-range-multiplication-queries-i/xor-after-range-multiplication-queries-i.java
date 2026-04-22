924d4884e362d6134591128e40f938263ccd4c49
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-22 12:47:49 +0530

    Time: 505 ms (5.75%) | Memory: 48.1 MB (29.71%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -1,20 +1,68 @@
 class Solution {
-    long MOD =(long)10e8+7;
-    public int xorAfterQueries(int[] nums, int[][] q) {
-        int n= nums.length;
-        long arr[] =new long[n];
-        for(int i=0;i<n;i++){
-            arr[i] = (long)nums[i];
+    private static final int MOD = 1_000_000_007;
+
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        Map<Integer, List<int[]>> groupQueryByK = new HashMap<>();
+
+        for (int[] query : queries) {
+            groupQueryByK
+                .computeIfAbsent(query[2], k -> new ArrayList<>())
+                .add(query);
         }
-        for(int i=0;i<q.length;i++){
-            for(int j=q[i][0];j<=q[i][1];j+=q[i][2]){
-                arr[j]=(arr[j]*(long)q[i][3])%MOD;
+
+        for (Map.Entry<Integer, List<int[]>> entry : groupQueryByK.entrySet()) {
+            int k = entry.getKey();
+            compute(entry.getValue(), k, nums);
+        }
+
+        int xor = 0;
+        for (int num : nums) xor ^= num;
+
+        return xor;
+    }
+
+    private void compute(List<int[]> queries, int k, int[] nums) {
+        int n = nums.length;
+        int[] diff = new int[n];
+
+        Arrays.fill(diff, 1);
+
+        for (int[] query : queries) {
+            int l = query[0], r = query[1], v = query[3];
+
+            diff[l] = (int)((long)diff[l] * v % MOD);
+
+            int steps = (r - l) / k;
+            int next = l + (steps + 1) * k;
+
+            if (next < n) {
+                diff[next] = (int)((long)diff[next] * modInverse(v) % MOD);
             }
         }
-        long ans =0;
-        for(int i=0;i<n;i++){
-            ans = ans^arr[i];
+
+        for (int i = k; i < n; i++) {
+            diff[i] = (int)((long)diff[i] * diff[i - k] % MOD);
         }
-        return (int)ans;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = (int)((long)nums[i] * diff[i] % MOD);
+        }
+    }
+
+    private int modPow(int base, int exp) {
+        long result = 1;
+        long b = base;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) result = (result * b) % MOD;
+            b = (b * b) % MOD;
+            exp >>= 1;
+        }
+
+        return (int) result;
+    }
+
+    private int modInverse(int x) {
+        return modPow(x, MOD - 2);
     }
 }
