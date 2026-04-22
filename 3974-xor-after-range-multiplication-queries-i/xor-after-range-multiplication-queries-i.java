9692612a92d560b3372e1a404f6f7ab6ce4f30e4
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-22 12:55:46 +0530

    Time: 487 ms (5.75%) | Memory: 48.1 MB (29.71%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -4,19 +4,18 @@ class Solution {
     public int xorAfterQueries(int[] nums, int[][] queries) {
         Map<Integer, List<int[]>> groupQueryByK = new HashMap<>();
 
-        for (int[] query : queries) {
-            groupQueryByK
-                .computeIfAbsent(query[2], k -> new ArrayList<>())
-                .add(query);
-        }
+        for(int[] query: queries)
+            groupQueryByK.computeIfAbsent(query[2], k -> new ArrayList<>()).add(query);
 
-        for (Map.Entry<Integer, List<int[]>> entry : groupQueryByK.entrySet()) {
-            int k = entry.getKey();
-            compute(entry.getValue(), k, nums);
+        for(List<int[]> queryGroup: groupQueryByK.values()){
+            int k = queryGroup.get(0)[2];
+            compute(queryGroup,k,nums);
         }
 
         int xor = 0;
-        for (int num : nums) xor ^= num;
+
+        for(int elem: nums)
+            xor ^= elem;
 
         return xor;
     }
@@ -48,6 +47,30 @@ class Solution {
             nums[i] = (int)((long)nums[i] * diff[i] % MOD);
         }
     }
+
+    // private void compute(List<int[]> queries, int k, int[] nums) {
+    //     int n = nums.length;
+    //     int[] diff = new int[n];
+
+    //     for(int i = 0; i < n; i++) diff[i] = 1;
+
+    //     for(int[] query: queries){
+    //         int l = query[0], r = query[1], v = query[3] % MOD;
+
+    //         diff[l] = ((diff[l] % MOD) * v) % MOD;
+
+    //         int steps = (r-l) / k;
+    //         int next = l + ((steps + 1) * k);
+    //         if(next < n)
+    //             diff[next] = ((diff[next] % MOD) * (modInverse(v) % MOD)) % MOD;
+    //     }
+
+    //     for(int i = k; i < n; i++)
+    //         diff[i] = ((diff[i] % MOD) * (diff[i-k] % MOD)) % MOD;
+
+    //     for(int i = 0; i < n; i++)
+    //         nums[i] = ((nums[i] % MOD) * (diff[i] % MOD)) % MOD;
+    // }
 
     private int modPow(int base, int exp) {
         long result = 1;
