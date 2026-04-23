2f44eb1b5900c178ba7ac9b351bc64db7f02c399
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-23 09:28:39 +0530

    Time: 22 ms (73.54%) | Memory: 122.4 MB (41.70%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -0,0 +1,47 @@
+class Solution {
+    public long[] distance(int[] nums) {
+        Map<Integer,List<Integer>> hm = new HashMap<>();
+        long[] distances = new long[nums.length];
+
+        for(int i = 0; i < nums.length; i++)
+            hm.computeIfAbsent(nums[i],k -> new ArrayList<>()).add(i);
+        
+        for(List<Integer> idxs: hm.values()) 
+            compute(distances,idxs);
+
+        return distances;
+    }
+
+    private void compute(long[] distance, List<Integer> idxs) {
+        
+        int n = idxs.size();
+        long[] prefixSum = new long[n];
+
+        if(n == 1) return;
+        
+        prefixSum[0] = idxs.get(0);
+
+        for(int i = 1; i < n; i++)
+            prefixSum[i] = prefixSum[i-1] + idxs.get(i);
+        
+        // System.out.println(Arrays.toString(prefixSum));
+
+        for(int i = 0; i < n; i++) {
+    
+            long prefixCurr = i * 1l * idxs.get(i);
+            long suffixCurr = (n-i-1) * 1l * idxs.get(i);
+
+            long prefixSumOfElem = i == 0 ? 0 : prefixSum[i-1];
+            long suffixSumOfElem = i == n-1 ? 0 : prefixSum[n-1] - prefixSum[i];
+
+            long prefixDistance = Math.abs(prefixCurr - prefixSumOfElem);
+            long suffixDistance = Math.abs(suffixCurr - suffixSumOfElem);
+
+            // System.out.println("Prefix Current elem: " + prefixCurr + "Suffix Current elem: " + suffixCurr);
+            // System.out.println("prefixSumOfElem: " + prefixSumOfElem + " suffixSumOfElem: " + suffixSumOfElem);
+            // System.out.println("prefixDistance: " + prefixDistance + " suffixDistance: " + suffixDistance);
+
+            distance[idxs.get(i)] = prefixDistance + suffixDistance;
+        }
+    }
+}
