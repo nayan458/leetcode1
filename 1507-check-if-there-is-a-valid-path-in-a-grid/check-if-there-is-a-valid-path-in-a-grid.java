7efa111d480e588690949b4ae592a62ba18baa13
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-28 09:36:54 +0530

    Time: 15 ms (85.79%) | Memory: 102.6 MB (32.92%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,42 +1,40 @@
 class Solution {
     private boolean[][] visited;
     private int m, n;
-
+    
     private int[][][] directions = {
-        {{0,-1},{0,1}},
+        {{0,-1},{0,1}}, 
         {{-1,0},{1,0}},
-        {{0,-1},{1,0}},
+        {{0,-1},{1,0}}, 
         {{1,0},{0,1}},
-        {{-1,0},{0,-1}},
+        {{-1,0},{0,-1}}, 
         {{-1,0},{0,1}}
     };
 
     public boolean hasValidPath(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        visited = new boolean[m][n];
-        return dfs(0, 0, -1, -1, grid);
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.visited = new boolean[m][n];
+        return dfs(0,0,-1,-1,grid);
     }
 
     private boolean dfs(int row, int col, int pr, int pc, int[][] grid) {
-        if (!isValid(row, col) || visited[row][col]) return false;
-
-        if (row == m - 1 && col == n - 1) return true;
+        if(!isValid(row,col) || visited[row][col])  return false;
+        if(row == m-1 && col == n-1)    return true;
 
         visited[row][col] = true;
 
         int street = grid[row][col] - 1;
 
-        for (int[] d : directions[street]) {
-            int nr = row + d[0];
-            int nc = col + d[1];
+        for(int[] direction: directions[street]){
+            int toRow = row + direction[0];
+            int toCol = col + direction[1];
+            if(!isValid(toRow,toCol) || visited[toRow][toCol]) continue;
+            if(pr == toRow && pc == toCol) continue;
 
-            if (!isValid(nr, nc) || visited[nr][nc]) continue;
-            if (pr == nr && pc == nc) continue;
+            if(!connectsBack(row, col, toRow, toCol, grid)) continue;
 
-            if (!connectsBack(row, col, nr, nc, grid)) continue;
-
-            if (dfs(nr, nc, row, col, grid)) return true;
+            if(dfs(toRow, toCol , row, col, grid)) return true;
         }
 
         return false;
@@ -44,17 +42,13 @@ class Solution {
 
     private boolean connectsBack(int fromRow, int fromCol, int toRow, int toCol, int[][] grid) {
         int street = grid[toRow][toCol] - 1;
-
-        for (int[] d : directions[street]) {
-            if (toRow + d[0] == fromRow && toCol + d[1] == fromCol) {
-                return true;
-            }
+        for(int[] direction: directions[street]){
+            if(toRow + direction[0] == fromRow && toCol + direction[1] == fromCol) return true;
         }
-
         return false;
     }
 
-    private boolean isValid(int r, int c) {
-        return r >= 0 && c >= 0 && r < m && c < n;
+    private boolean isValid(int row, int col) {
+        return row >= 0 && col >= 0 && row < m && col < n;
     }
 }
