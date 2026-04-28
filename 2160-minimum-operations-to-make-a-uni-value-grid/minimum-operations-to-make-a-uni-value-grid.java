1591fc5a3c5e9bb67ec6e80afb9e47f3be4937d3
Nayanmoni Baruah <nayancoding@gmail.com>
2026-04-28 10:39:04 +0530

    Time: 47 ms (58.78%) | Memory: 110.6 MB (55.72%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int m = grid.length, n = grid[0].length, i = 0;
+
+        int[] arr = new int[m*n];
+
+        for(int[] row: grid)
+            for(int elem: row)
+                arr[i++] = elem;
+            
+        Arrays.sort(arr);
+
+        if(!isNotPossible(arr, x)) return -1;
+
+        int r = arr.length;
+        int mid = r/2;
+        int opp = calcOpp(arr,mid,x);
+        if(r % 2 == 0)
+            opp = Math.min(opp,calcOpp(arr,mid-1,x));
+        return opp;
+    }
+
+    private int calcOpp(int[] arr, int mid, int x) {
+        int midElem = arr[mid];
+        int opp = 0;
+        for(int elem: arr)
+            opp += (int)(Math.abs(elem - midElem) / x);
+        // System.out.println("midElem: " + midElem);
+        // System.out.println("opp: " + opp);
+        return opp;
+    }
+
+    private boolean isNotPossible(int[] arr, int x) {
+        if(x == 1) return true;
+        int remainder = arr[0] % x;
+        for(int elem: arr)
+            if(elem % x != remainder) return false;
+        return true;
+    }
+}
