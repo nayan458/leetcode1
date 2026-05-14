c3f9a70212a5f13f6f314877b8916fe625f30d04
Nayanmoni Baruah <nayancoding@gmail.com>
2026-05-14 10:33:33 +0530

    Time: 1 ms (91.05%) | Memory: 44.7 MB (64.57%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,15 +1,14 @@
 class Solution {
-    private int[] visited;
+    
 
     public boolean isGood(int[] nums) {
         int n = nums.length;
-        this.visited = new int[n];
+        int[] visited = new int[n];
 
         for(int num: nums) {
             if(num > n-1 || (visited[num] == 1 && num != n-1) || (num == n-1 && visited[num] == 2)) return false;
             visited[num]++;
         }
-
         return true;
     }
 }
