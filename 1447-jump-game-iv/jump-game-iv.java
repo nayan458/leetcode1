6769dcac8384dc8ad470640d6add377e9808d474
Nayanmoni Baruah <nayancoding@gmail.com>
2026-05-18 11:33:17 +0530

    Time: 61 ms (40.56%) | Memory: 82.5 MB (39.96%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -2,25 +2,22 @@ class Solution {
 
     public int minJumps(int[] arr) {
         int n = arr.length;
-        Map<Integer,Set<Integer>> hm = new HashMap<>();
+        Map<Integer,List<Integer>> hm = new HashMap<>();
 
         for(int i = 0; i < n; i++)
-            hm.computeIfAbsent(arr[i],k -> new HashSet<>()).add(i);
-
-        // create graph
-        // List<Set<Integer>> adjList = createGraph(hm, arr);
+            hm.computeIfAbsent(arr[i],k -> new ArrayList<>()).add(i);
 
         return bfs(arr,hm);
     }
 
-    private int bfs(int[] arr, Map<Integer,Set<Integer>> hm) {
+    private int bfs(int[] arr, Map<Integer,List<Integer>> hm) {
         
         int n = arr.length;
         Deque<Integer> q = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
         Set<Integer> set = new HashSet<>();
         visited[0] = true;
-        q.addLast(0);
+        q.offer(0);
         int jump = -1;
 
         while(!q.isEmpty()) {
@@ -29,77 +26,29 @@ class Solution {
             int size = q.size();
 
             while(size-- > 0){
-                int idx = q.removeFirst();
+                int idx = q.poll();
                 if(idx == n-1) return jump;
 
                 if(!set.contains(arr[idx])) 
                     for(int neighbour: hm.get(arr[idx])) {
                         if(!visited[neighbour]){
                             visited[neighbour] = true;
-                            q.addLast(neighbour);
+                            q.offer(neighbour);
                         }
                     }
                 set.add(arr[idx]);
                 
                 if(idx > 0 && !visited[idx-1]){
-                    q.addLast(idx-1);
+                    q.offer(idx-1);
                     visited[idx-1] = true;
                 }
                 if(idx < n-1 && !visited[idx+1]){
-                    q.addLast(idx+1);
+                    q.offer(idx+1);
                     visited[idx+1] = true;
                 }
             }
         }
 
-        return n-1;
+        return -1;
     }   
-
-    // private List<Set<Integer>> createGraph(HashMap<Integer,Set<Integer>> hm, int[] arr) {
-    //     List<Set<Integer>> adjList = new ArrayList<>();
-        
-    //     for(int i = 0; i < n; i++) {
-    //         adjList.add(new HashSet<>());
-    //         if(i > 0)
-    //             adjList.get(i).add(i-1);
-    //         if(i < n-1)
-    //             adjList.get(i).add(i+1);
-    //     }
-
-    //     for(int u = 0; u < n; u++) {
-    //         for(int v: hm.get(arr[u])) {
-    //             adjList.get(u).add(v);
-    //             adjList.get(v).add(u);
-    //         }
-    //         set.add(arr[u]);
-    //     }
-    //     return adjList;
-    // }
-
-    // private int bfs(int[] arr,List<Set<Integer>> adjList) {
-        
-    //     int n = arr.length;
-    //     Deque<int[]> q = new ArrayDeque<>();
-    //     boolean[] visited = new boolean[n];
-
-    //     visited[0] = true;
-    //     q.addLast(new int[]{0,0});
-
-    //     while(!q.isEmpty()) {
-    //         int[] node = q.removeFirst();
-    //         int idx = node[0];
-    //         int jump = node[1];
-
-    //         if(idx == n-1) return jump;
-
-    //         for(int neighbour: adjList.get(idx)) {
-    //             if(!visited[neighbour]){
-    //                 visited[neighbour] = true;
-    //                 q.addLast(new int[]{neighbour,jump+1});
-    //             }
-    //         }
-    //     }
-
-    //     return n-1;
-    // }   
 }
