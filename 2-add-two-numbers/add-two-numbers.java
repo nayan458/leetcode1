19463db0add516a0f00d1c5c68edd4a602656297
Nayanmoni Baruah <nayancoding@gmail.com>
2026-09-06 14:32:04 +0530

    Time: 30 ms (1.69%) | Memory: 47.2 MB (6.65%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,38 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
-        ListNode ptr = l1, pre_ptr = ptr;
-        while(ptr != null && l2 != null) {
-            ptr.val = (ptr.val + l2.val + carry);
-            if(ptr.val > 9) {
-                carry = ptr.val/10;
-                ptr.val %= 10;
-            } else
-                carry = 0;
-            pre_ptr = ptr;
-            ptr = ptr.next;
-            l2 = l2.next;
-        }
-        
-        if(l2 != null || ptr != null){
-            pre_ptr.next = l2 != null ? l2 : ptr;
-            ptr = l2 != null? l2 : ptr;
-            while(carry != 0 && ptr != null){
-                ptr.val += carry;
-                carry = ptr.val/10;
-                ptr.val %= 10;
+        int sum = 0;
 
-                pre_ptr = ptr;
-                ptr = ptr.next;
-            }
+        ListNode dummy = new ListNode(0);
+        ListNode dummyHead = dummy;
+        
+        while(l1 != null && l2 != null) {
+            sum = l1.val + l2.val + carry;
+            carry = sum/10;
+            dummy.next = new ListNode(sum % 10);
+            l1 = l1.next;
+            l2 = l2.next;
+            dummy = dummy.next;
+            System.out.println(sum % 10);
+        }
+
+        while(l1 != null) {
+            sum = l1.val + carry;
+            carry = sum/10;
+            dummy.next = new ListNode(sum % 10);
+            l1 = l1.next;
+            dummy = dummy.next;
+            System.out.println(sum % 10);
+        }
+
+        while(l2 != null) {
+            sum = l2.val + carry;
+            carry = sum/10;
+            dummy.next = new ListNode(sum % 10);
+            l2 = l2.next;
+            dummy = dummy.next;
+            System.out.println(sum % 10);
         }
 
         if(carry != 0)
-            pre_ptr.next = new ListNode(carry);
+            dummy.next = new ListNode(1);
+        
 
-
-
-
-        return l1;
+        return dummyHead.next;
     }
 }
