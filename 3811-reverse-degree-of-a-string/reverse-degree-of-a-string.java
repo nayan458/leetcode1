e6f889a07b727e90f206a1ec3569d94caf45b404
Nayanmoni Baruah <nayancoding@gmail.com>
2026-09-20 21:45:58 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (67.44%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -3,7 +3,6 @@ class Solution {
         int count = 0;
         for(int i = 0; i < s.length(); i++)
             count += (26 - (int)(s.charAt(i) - 'a')) * (i+1);
-            // System.out.println(26 - (int)(s.charAt(i) - 'a'));
         return count;
     }
 }
